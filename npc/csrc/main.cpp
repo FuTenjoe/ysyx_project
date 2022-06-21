403a887c7174d1ec36f46d3cc19ceded7b41bea7
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-21 22:05:40 +0800

    >  sim RTL ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.13.0-51-generic #58~20.04.1-Ubuntu SMP Tue Jun 14 11:29:12 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:05:40 up  2:24,  1 user,  load average: 0.00, 0.03, 0.01

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -137,7 +137,7 @@ int main(int argc, char **argv, char **env) {
     }
     if(main_time < 4){
       top->rst = 1;
-      init_difftest(img_size,port);
+      //init_difftest(img_size,port);
     }
     else{
       top->rst = 0;
