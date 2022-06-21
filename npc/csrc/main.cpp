0618f54445a1aae7d6bc86e6076ef533100c914d
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-21 21:02:10 +0800

    >  sim RTL ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.13.0-51-generic #58~20.04.1-Ubuntu SMP Tue Jun 14 11:29:12 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:02:10 up  1:29,  1 user,  load average: 0.29, 0.09, 0.03

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -244,6 +244,10 @@ void init_difftest(long img_size,int port){
   assert(ref_so_file != NULL);
   void *handle;
   handle = dlopen(ref_so_file,RTLD_LAZY); //将动态库加载到内存中
+  if(handle == NULL){
+    printf("库打开失败！\n")；
+    return 0;
+  }
   assert(handle);
   //用函数指针指向动态库中的对应函数，以便调用
   ref_difftest_memcpy = (void(*)(paddr_t, void*, size_t, bool))dlsym(handle,"difftest_memcpy");
