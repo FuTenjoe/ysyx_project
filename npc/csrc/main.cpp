ca7db084430928ea33fa0d9c4b391e92a1aae1ab
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-23 16:12:41 +0800

    >  sim RTL ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.13.0-51-generic #58~20.04.1-Ubuntu SMP Tue Jun 14 11:29:12 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:12:41 up  3:52,  0 users,  load average: 0.00, 0.05, 0.05

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -145,8 +145,8 @@ void init_imem(){
 
 uint8_t *guest_to_host(paddr_t paddr){
   uint8_t *tmpl = pimem + paddr -CONFIG_MBASE;
-  printf("guest to host success pimem = %hhn\n",pimem);
-   printf("guest to host success paddr  = %d\n",paddr );
+  //printf("guest to host success pimem = %hhn\n",pimem);
+  //printf("guest to host success paddr  = %d\n",paddr );
   printf("guest to host success addr = %hhn\n",tmpl);
   return tmpl;
 }
