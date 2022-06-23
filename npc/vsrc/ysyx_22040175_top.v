f7c41455437d6f9e673b3101fc457f991ecfad37
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-23 16:33:53 +0800

    >  sim RTL ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.13.0-51-generic #58~20.04.1-Ubuntu SMP Tue Jun 14 11:29:12 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:33:53 up  4:13,  0 users,  load average: 0.00, 0.08, 0.06

--- npc/vsrc/ysyx_22040175_top.v
+++ npc/vsrc/ysyx_22040175_top.v
@@ -111,13 +111,13 @@ alu u_alu_0(
 
 //wire [63:0] rdata;
 
-import "DPI-C" function void pmem_read(input longint raddr, output longint rdata);
+/*import "DPI-C" function void pmem_read(input longint raddr, output longint rdata);
 import "DPI-C" function void pmem_write(input longint waddr, input longint wdata, input byte wmask);
 wire [63:0] rdata;
 always @(*) begin
   pmem_read(pc, rdata);
   pmem_write(pc, wdata, wmask);
-end
+end*/
 
 
 endmodule
