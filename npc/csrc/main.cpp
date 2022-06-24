445b543fcf88f500acfc5464e8348399d2f388d2
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-24 10:40:20 +0800

    >  sim RTL ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.13.0-51-generic #58~20.04.1-Ubuntu SMP Tue Jun 14 11:29:12 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:40:20 up  5:45,  0 users,  load average: 0.10, 0.03, 0.00

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -136,6 +136,7 @@ int main(int argc, char **argv, char **env) {
       printf("main_time = %d\n",i);
        printf("PC:0x%0x;Inst:0x%x;\n",top->pc,top->inst);
       a= a+1;
+      printf("a =%d \n",a);
      }
      if (a>2){
        difftest_step(top->pc);
