35179de4f56acc3f93c57060ca4477639604e220
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-24 21:10:00 +0800

    >  sim RTL ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.13.0-51-generic #58~20.04.1-Ubuntu SMP Tue Jun 14 11:29:12 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:10:00 up 11:34,  0 users,  load average: 0.07, 0.05, 0.01

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -124,11 +124,12 @@ int main(int argc, char **argv, char **env) {
     }
     else{
       top->rst = 0;
-      top->inst = pmem_read(top->pc,8);
+      
     }
     if(main_time % 2 == 0){
       top->clk = 0;
-      
+      if(main_time >3 ){
+      top->inst = pmem_read(top->pc,8);}
       top->eval();
       printf("main_time = %ld\n",main_time);
       printf("PC:0x%0x;Inst:0x%x;\n",top->pc,top->inst);
