48aeae5e63730837ae2acfae2f523de32f381575
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-24 21:24:45 +0800

    >  sim RTL ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.13.0-51-generic #58~20.04.1-Ubuntu SMP Tue Jun 14 11:29:12 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:24:45 up 11:49,  0 users,  load average: 0.10, 0.05, 0.00

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -144,7 +144,7 @@ int main(int argc, char **argv, char **env) {
       printf(" npc_gpr[%d]= 0x%08lx; Instruction is 0x%x\n",1,cpu_gpr[1],top->inst);
       a= a+1;
 
-      if (a>1){
+      if (a>2){
        //printf("a =%d \n",a);
        
        difftest_step(top->pc);
