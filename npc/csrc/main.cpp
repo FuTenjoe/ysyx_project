60455502bdaab364e831eaf41312322d5c4473ce
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-24 10:31:43 +0800

    >  sim RTL ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.13.0-51-generic #58~20.04.1-Ubuntu SMP Tue Jun 14 11:29:12 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:31:43 up  5:37,  0 users,  load average: 0.00, 0.02, 0.00

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -146,11 +146,13 @@ int main(int argc, char **argv, char **env) {
       }
      
   }
-  return is_exit_status_bad();
-  if (Verilated::gotFinish())  exit(0);
+  
+
   tfp->close();
   delete top;
-  exit(0);
+  delete contextp;
+  return is_exit_status_bad();
+  if (Verilated::gotFinish())  exit(0);
   
 }
 
