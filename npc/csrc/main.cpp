89200ee5a6911edbee8988b9a83e1d2f3b407058
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-24 21:00:40 +0800

    >  sim RTL ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.13.0-51-generic #58~20.04.1-Ubuntu SMP Tue Jun 14 11:29:12 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:00:40 up 11:25,  0 users,  load average: 0.14, 0.08, 0.02

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -100,10 +100,11 @@ int main(int argc, char **argv, char **env) {
   Verilated::traceEverOn(true);
   top->trace (tfp, 99);
   tfp->open ("Vysyx_22040175.vcd");
-  top->inst = pmem_read(top->pc,8);
-  //npc_state = NPC_RUNNING;
   char* img_file = *(argv + 1);
   long img_size = load_img(img_file);
+  top->inst = pmem_read(top->pc,8);
+  //npc_state = NPC_RUNNING;
+  
   //while(!contextp -> gotFinish()){
   while(main_time < 10){
     
