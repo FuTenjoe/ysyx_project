e73cb3a95d1815a8f83445296aa4ea3fb4543b07
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-25 01:05:39 +0800

    >  sim RTL ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.13.0-51-generic #58~20.04.1-Ubuntu SMP Tue Jun 14 11:29:12 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  01:05:39 up 15:07,  0 users,  load average: 0.09, 0.07, 0.02

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -138,7 +138,14 @@ int main(int argc, char **argv, char **env) {
       tfp->dump (2*i+clk);
       top->clk = !top->clk;
       top->eval ();
-      
+      if (a>2){
+       //printf("a =%d \n",a);
+       
+       difftest_step(top->pc);
+     }
+     else{
+      init_difftest(img_size,port);
+     }
   }
   
      if(top->clk==1){
@@ -148,14 +155,7 @@ int main(int argc, char **argv, char **env) {
       printf(" npc_gpr[%d]= 0x%08lx; Instruction is 0x%x\n",10,cpu_gpr[10],top->inst);
       printf(" npc_gpr[%d]= 0x%08lx; Instruction is 0x%x\n",1,cpu_gpr[1],top->inst);
       a= a+1;
-      if (a>2){
-       //printf("a =%d \n",a);
-       
-       difftest_step(top->pc);
-     }
-     else{
-      init_difftest(img_size,port);
-     }
+      
      }
      
       if(npc_state == NPC_ABORT){
