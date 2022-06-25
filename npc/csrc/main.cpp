18ee600e4b18b443b08449dddb181966fc678a7f
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-26 03:26:01 +0800

    >  sim RTL ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.13.0-51-generic #58~20.04.1-Ubuntu SMP Tue Jun 14 11:29:12 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  03:26:01 up  7:56,  1 user,  load average: 0.06, 0.03, 0.00

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -63,7 +63,7 @@ void difftest_step(vaddr_t pc);
 static void checkregs(CPU_state *ref, vaddr_t pc);
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc);
 //static inline void host_write(void *addr, int len, word_t data);
-void host_write(void *addr, int len, word_t data);
+static inline void host_write(void *addr, int len, word_t data);
 
 //加为DPIC函数
 extern "C" void pmem_read(long long raddr,long long *rdata){
