9583496288348436a1a7bca4932727b3ee6645c4
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-25 15:49:00 +0800

    >  sim RTL ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.13.0-51-generic #58~20.04.1-Ubuntu SMP Tue Jun 14 11:29:12 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:49:00 up  1:15,  1 user,  load average: 0.18, 0.10, 0.03

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -147,7 +147,7 @@ int main(int argc, char **argv, char **env) {
   
   long img_size = load_img(img_file);
   int i;
-  for (i=0; ; i++) {
+  for (i=0;i<15 ; i++) {
     //while(!contextp -> gotFinish()){
      //for (i=0;i<15 ; i++) {
       
@@ -179,19 +179,19 @@ int main(int argc, char **argv, char **env) {
       printf("PC:0x%0x;Inst:0x%x;\n",top->pc,top->inst);
       printf(" npc_gpr[%d]= 0x%08lx; Instruction is 0x%x\n",10,cpu_gpr[10],top->inst);
       printf(" npc_gpr[%d]= 0x%08lx; Instruction is 0x%x\n",1,cpu_gpr[1],top->inst);
-      a= a+1;
-      if (a>1){
-       //printf("a =%d \n",a);
-       
-         difftest_step(top->pc);
-        }
-        else{
-          init_difftest(img_size,port);
-        }
+      
       
      }
      if(top->clk==0){
-        
+        a= a+1;
+      if (a>1){
+       //printf("a =%d \n",a);
+       
+         //difftest_step(top->pc);
+        }
+        else{
+          //init_difftest(img_size,port);
+        }
         
      }
      
