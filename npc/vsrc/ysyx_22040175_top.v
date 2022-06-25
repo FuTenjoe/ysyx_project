a29f0fbfa663627cdb568844ecf0d2ae988026d2
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-25 15:26:37 +0800

    >  sim RTL ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.13.0-51-generic #58~20.04.1-Ubuntu SMP Tue Jun 14 11:29:12 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:26:37 up 52 min,  1 user,  load average: 0.00, 0.01, 0.02

--- npc/vsrc/ysyx_22040175_top.v
+++ npc/vsrc/ysyx_22040175_top.v
@@ -8,8 +8,8 @@ module  ysyx_22040175_top(
     output [`CPU_WIDTH-1:0]       unknown_code
    // output[`CPU_WIDTH-1:0]        next_pc
 );
-wire  [31:0]                 inst,
-wire[`CPU_WIDTH-1:0]        pc,
+reg  [31:0]                 inst,
+reg[`CPU_WIDTH-1:0]        pc,
 wire rst_n;
 assign rst_n = !rst;
 wire                         ena;
