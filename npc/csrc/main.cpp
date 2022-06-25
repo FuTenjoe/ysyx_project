549d40c5fcaba740a0560c014a3ff8585979bfae
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-26 03:32:41 +0800

    >  sim RTL ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.13.0-51-generic #58~20.04.1-Ubuntu SMP Tue Jun 14 11:29:12 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  03:32:41 up  8:02,  1 user,  load average: 0.04, 0.05, 0.00

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -108,6 +108,7 @@ extern "C" void pmem_write(long long waddr,long long wdata,char wmask){
   printf("len = %d\n",len);
   long long *rdata;
   *rdata= host_read(guest_to_host(waddr), 8);
+  printf("rdata = 0x%llx\n",*rdata);
   *rdata = wdata;
   host_write(guest_to_host(waddr),len,wdata);
 }
