94924779bc099a07d44be1deab2bb5f8cff10299
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-26 01:39:52 +0800

    >  sim RTL ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.13.0-51-generic #58~20.04.1-Ubuntu SMP Tue Jun 14 11:29:12 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  01:39:52 up  6:13,  1 user,  load average: 0.00, 0.03, 0.00

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -240,11 +240,11 @@ inline word_t host_read(void *addr, int len) {
 }
 static inline void host_write(void *addr, int len,word_t data){
   switch (len){
-    case 1: *(uint8_t *)addr = data; return;
-    case 2: *(uint16_t *)addr = data; return;
-    case 4: *(uint32_t *)addr = data;return;
-    case 8: *(uint64_t *)addr = data;printf("hoost_write ok!\n");return;
-    default:{printf("hoost_write is error !\n"); assert(0);};
+    case 1: return *(uint8_t *)addr = data; 
+    case 2: return *(uint16_t *)addr = data; 
+    case 4: return *(uint32_t *)addr = data;
+    case 8: return *(uint64_t *)addr = data;printf("host_write ok!\n");
+    default:{printf("host_write is error !\n"); assert(0);};
   }
 }
 
