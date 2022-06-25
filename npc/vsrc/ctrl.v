7e71c064212fb7db6877c0d6b773cf5f2cf08ab6
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-25 21:50:35 +0800

    >  sim RTL ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.13.0-51-generic #58~20.04.1-Ubuntu SMP Tue Jun 14 11:29:12 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:50:35 up  4:53,  1 user,  load average: 0.01, 0.03, 0.00

--- npc/vsrc/ctrl.v
+++ npc/vsrc/ctrl.v
@@ -114,15 +114,15 @@ always @(*) begin
                 alu_op      = `ALU_ADD;
                 alu_src_sel = `ALU_SRC_IMM; // x0 + imm
         end
-       // `INST_AUIPC:begin //only auipc
-            /*    reg_wen     = 1'b1;
+        `INST_AUIPC:begin //only auipc
+               reg_wen     = 1'b1;
                 reg1_raddr  = `REG_ADDR_WIDTH'b0; // x0 = 0
                 reg_waddr   = rd;
                 imm_gen_op  = `IMM_GEN_U;
                 alu_op      = `ALU_ADD;
                 alu_src_sel = `ALU_SRC_IMM_PC; // x0 + imm
-                //wmask = 8'b11111111;*/
-        //end
+                wmask = 8'b11111111;
+        end
         7'b1100111:begin
             case(funct3)  
                 3'b000:begin         //jalr
