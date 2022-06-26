0f00a4815f659cd3c18dc65e3a7ee48309c7e643
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-26 18:45:07 +0800

    >  sim RTL ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.13.0-51-generic #58~20.04.1-Ubuntu SMP Tue Jun 14 11:29:12 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:45:07 up  4:44,  1 user,  load average: 0.08, 0.06, 0.01

--- npc/vsrc/ctrl.v
+++ npc/vsrc/ctrl.v
@@ -91,7 +91,8 @@ always @(*) begin
                     alu_src_sel = `ALU_SRC_IMM;
                     wmask =  8'b0;
                     s_flag = 1'd0;
-                    expand_signed =4'd2;    //截断为32位     
+                    expand_signed =4'd0;    //截断为32位  
+                end   
                 default:unknown_code = inst;
             endcase
         end
