d3ac4f2239725e410c17cb0a1990171d45d3d456
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-26 22:28:21 +0800

    >  sim RTL ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.13.0-51-generic #58~20.04.1-Ubuntu SMP Tue Jun 14 11:29:12 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:28:21 up  7:10,  1 user,  load average: 0.02, 0.03, 0.00

--- npc/obj_dir/Vysyx_22040175_top___024root.cpp
+++ npc/obj_dir/Vysyx_22040175_top___024root.cpp
@@ -1743,9 +1743,7 @@ VL_INLINE_OPT void Vysyx_22040175_top___024root___sequent__TOP__2(Vysyx_22040175
                                                      : 
                                                     (((IData)(vlSelf->ysyx_22040175_top__DOT__jump) 
                                                       & (IData)(vlSelf->ysyx_22040175_top__DOT__jalr))
-                                                      ? 
-                                                     ((IData)(vlSelf->ysyx_22040175_top__DOT__jalr_buf) 
-                                                      + vlSelf->ysyx_22040175_top__DOT__imm)
+                                                      ? (IData)(vlSelf->ysyx_22040175_top__DOT__jalr_buf)
                                                       : 
                                                      ((IData)(vlSelf->ysyx_22040175_top__DOT__ebreak_flag)
                                                        ? 0x80000000U
