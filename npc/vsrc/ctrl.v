52c460c2c1222beb7f1c6ad121e1b7a7e07309b2
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-28 01:14:32 +0800

    >  sim RTL ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.13.0-51-generic #58~20.04.1-Ubuntu SMP Tue Jun 14 11:29:12 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  01:14:32 up 59 min,  1 user,  load average: 0.02, 0.01, 0.00

--- npc/vsrc/ctrl.v
+++ npc/vsrc/ctrl.v
@@ -64,7 +64,7 @@ always @(*) begin
                 `INST_ADD_SUB: begin
                     alu_op = (funct7 == `FUNCT7_INST_A) ? `ALU_ADD : `ALU_SUB; // A:add B:sub 
                     s_flag = 1'd0;
-                    expand_signed = 4'd2;
+                    expand_signed = 4'd0;
                 end
                 default:unknown_code = inst;
             endcase
