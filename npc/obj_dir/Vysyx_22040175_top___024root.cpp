ad99cdb0b0705fad260f438d37779a7f72cf6cd2
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-29 00:08:35 +0800

    >  sim RTL ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.13.0-51-generic #58~20.04.1-Ubuntu SMP Tue Jun 14 11:29:12 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  00:08:35 up  1:08,  1 user,  load average: 0.05, 0.03, 0.00

--- npc/obj_dir/Vysyx_22040175_top___024root.cpp
+++ npc/obj_dir/Vysyx_22040175_top___024root.cpp
@@ -412,7 +412,7 @@ VL_INLINE_OPT void Vysyx_22040175_top___024root___sequent__TOP__1(Vysyx_22040175
                     vlSelf->ysyx_22040175_top__DOT__expand_signed = 0U;
                 } else if ((5U == (7U & (IData)(((QData)((IData)(vlSelf->inst)) 
                                                  >> 0xcU))))) {
-                    vlSelf->ysyx_22040175_top__DOT__expand_signed = 3U;
+                    vlSelf->ysyx_22040175_top__DOT__expand_signed = 0U;
                 }
             }
         }
--- npc/obj_dir/Vysyx_22040175_top___024root__Slow.cpp
+++ npc/obj_dir/Vysyx_22040175_top___024root__Slow.cpp
@@ -310,7 +310,7 @@ void Vysyx_22040175_top___024root___settle__TOP__2(Vysyx_22040175_top___024root*
                     vlSelf->ysyx_22040175_top__DOT__expand_signed = 0U;
                 } else if ((5U == (7U & (IData)(((QData)((IData)(vlSelf->inst)) 
                                                  >> 0xcU))))) {
-                    vlSelf->ysyx_22040175_top__DOT__expand_signed = 3U;
+                    vlSelf->ysyx_22040175_top__DOT__expand_signed = 0U;
                 }
             }
         }
