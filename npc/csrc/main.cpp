71a71e386d5b6c7d6793ae9c6c383d4666e34471
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-29 16:37:30 +0800

    >  sim RTL ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.13.0-51-generic #58~20.04.1-Ubuntu SMP Tue Jun 14 11:29:12 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:37:30 up 14 min,  1 user,  load average: 0.00, 0.18, 0.24

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -185,7 +185,7 @@ int main(int argc, char **argv, char **env) {
       printf("PC:0x%0x;Inst:0x%x;\n",top->pc,top->inst);
       printf(" npc_gpr[%d]= 0x%08lx; Instruction is 0x%x\n",10,cpu_gpr[10],top->inst);
       printf(" npc_gpr[%d]= 0x%08lx; Instruction is 0x%x\n",23,cpu_gpr[23],top->inst);
-      printf(" npc_gpr[%d]= 0x%08lx; Instruction is 0x%x\n",15,cpu_gpr[15],top->inst);
+      printf(" npc_gpr[%d]= 0x%08lx; Instruction is 0x%x\n",2,cpu_gpr[2],top->inst);
       
       
      }
@@ -198,7 +198,7 @@ int main(int argc, char **argv, char **env) {
       
       printf(" npc_gpr[%d]= 0x%08lx; Instruction is 0x%x\n",10,cpu_gpr[10],top->inst);
       printf(" npc_gpr[%d]= 0x%08lx; Instruction is 0x%x\n",23,cpu_gpr[23],top->inst);
-      printf(" npc_gpr[%d]= 0x%08lx; Instruction is 0x%x\n",11,cpu_gpr[11],top->inst);
+      printf(" npc_gpr[%d]= 0x%08lx; Instruction is 0x%x\n",2,cpu_gpr[2],top->inst);
       printf(" npc_gpr[%d]= 0x%08lx; Instruction is 0x%x\n",15,cpu_gpr[15],top->inst);
    
         if (a>2){
