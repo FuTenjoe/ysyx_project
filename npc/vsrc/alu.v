9cf15a6f466ca2518e9535d1497ad10941ba4e76
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-29 22:48:33 +0800

    >  sim RTL ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.13.0-51-generic #58~20.04.1-Ubuntu SMP Tue Jun 14 11:29:12 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:48:33 up  5:44,  1 user,  load average: 0.00, 0.02, 0.00

--- npc/vsrc/alu.v
+++ npc/vsrc/alu.v
@@ -82,7 +82,7 @@ always @(*) begin
         `ALU_DIVYW:begin
                 signed_alu_src1 = alu_src1[31:0];
                 signed_alu_src2 = alu_src2[31:0];
-                alu_res = signed_alu_src1 %  signed_alu_src2 ;    //不确定
+                alu_res = signed_alu_src1[31:0] %  signed_alu_src2[31:0] ;    //不确定
         end
         default:begin
             alu_res = alu_src1 -  alu_src2;
