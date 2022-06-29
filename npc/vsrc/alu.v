0c6bbde2e10eeeaeabf31dc7ecb7f9cd5cc2e521
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-29 22:38:28 +0800

    >  sim RTL ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.13.0-51-generic #58~20.04.1-Ubuntu SMP Tue Jun 14 11:29:12 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:38:28 up  5:33,  1 user,  load average: 0.02, 0.05, 0.01

--- npc/vsrc/alu.v
+++ npc/vsrc/alu.v
@@ -80,7 +80,7 @@ always @(*) begin
         `ALU_DIVW:
                 alu_res = alu_src1[31:0] / alu_src2[31:0];
         `ALU_DIVYW:
-                alu_res = alu_src1[31:0] % alu_src2[31:0] ;    //不确定
+                alu_res = alu_src1[31:0]  ;    //不确定
         default:begin
             alu_res = alu_src1 -  alu_src2;
             zero = (alu_res == `CPU_WIDTH'b0) ? 1'b1 : 1'b0;
