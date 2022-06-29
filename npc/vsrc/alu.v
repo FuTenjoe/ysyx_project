e8d14f1482d2dbb27d4fab81013c33aa8c6cae80
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-29 17:32:58 +0800

    >  sim RTL ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.13.0-51-generic #58~20.04.1-Ubuntu SMP Tue Jun 14 11:29:12 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:32:58 up  1:09,  1 user,  load average: 0.00, 0.01, 0.00

--- npc/vsrc/alu.v
+++ npc/vsrc/alu.v
@@ -57,7 +57,7 @@ always @(*) begin
 end
 import "DPI-C" function void pmem_read(input longint raddr, output longint rdata);
 always @(*) begin
-    if(rd_flag == 3'd1 || rd_flag == 3'd2||rd_flag == 3'd4)
+    if(rd_flag == 3'd1 | rd_flag == 3'd2 |rd_flag == 3'd4)
   pmem_read(alu_src1 +  alu_src2, rd_buf_lw);
   
 end
