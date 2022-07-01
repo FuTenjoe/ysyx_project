5234ea894359acce4cc750c78857d25d8c050fa1
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-01 19:47:40 +0800

    >  compile NEMU ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.13.0-51-generic #58~20.04.1-Ubuntu SMP Tue Jun 14 11:29:12 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:47:40 up  6:57,  1 user,  load average: 0.14, 0.13, 0.06

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -10,7 +10,7 @@
  */
 #define MAX_INST_TO_PRINT 10
 
-CPU_state cpu = {};
+CPU_state cpu = {.mstatus=0xa00001800};  //自己加实现自陷
 uint64_t g_nr_guest_inst = 0;
 static uint64_t g_timer = 0; // unit: us
 static bool g_print_step = false;
