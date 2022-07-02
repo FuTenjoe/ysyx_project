ef133823cb0ab6e42ad0c9829e180c4c8e59c1b1
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-02 22:35:06 +0800

    >  compile NEMU ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.13.0-51-generic #58~20.04.1-Ubuntu SMP Tue Jun 14 11:29:12 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:35:06 up  3:36,  0 users,  load average: 0.15, 0.06, 0.01

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -11,7 +11,7 @@ Context* __am_irq_handle(Context *c) {
     switch (c->mcause) {
       default: ev.event = EVENT_ERROR; break;
     }
-    printf("mcause = %llx\n",c->mcause);
+    printf("mcause = %d\n",c->mcause);
     printf("mstatus = %llx\n",c->mstatus);
     printf("mepc = %llx\n",c->mepc);
     c = user_handler(ev, c);
