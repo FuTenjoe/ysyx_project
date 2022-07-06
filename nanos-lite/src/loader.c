b7e3b2973c1885cd151d7a41b9c95a80d76df79d
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-06 21:54:01 +0800

    >  compile NEMU ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.13.0-51-generic #58~20.04.1-Ubuntu SMP Tue Jun 14 11:29:12 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:54:01 up  2:01,  1 user,  load average: 0.01, 0.04, 0.00

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -18,8 +18,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   //return 0;
   //自己加
   printf("eok\n");
-  ramdisk_read(ehdr, 0, get_ramdisk_size());
-  printf("eok2\n");
+  ramdisk_read(ehdr, ehdr->e_phoff, get_ramdisk_size());
   //assert(*(uint32_t *)ehdr->e_ident == 0x7f454c46);
   assert(*(uint32_t *)ehdr->e_ident == 0x464c457f);
   printf("eok2\n");
