c290e6f06f9f1c12db773b551f8cfff2f32aa83d
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-07 13:19:07 +0800

    >  compile NEMU ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.13.0-52-generic #59~20.04.1-Ubuntu SMP Thu Jun 16 21:21:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:19:07 up  2:17,  1 user,  load average: 0.14, 0.10, 0.02

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -37,16 +37,16 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
     printf("eok32\n");
   }
   }*/
-  //Elf_Ehdr *elf_head = (Elf_Ehdr*)malloc(sizeof(Elf_Ehdr));
+  Elf_Ehdr *elf_head = (Elf_Ehdr*)malloc(sizeof(Elf_Ehdr));
   printf("loderok1\n");
-  Elf_Ehdr *elf_head =NULL;
+  //Elf_Ehdr *elf_head =NULL;
   ramdisk_read(elf_head,0,get_ramdisk_size());
   printf("loderok1\n");
   assert(*(uint32_t *)ehdr.e_ident == 0x464c457f);
   assert(elf_head -> e_machine == PT_LOAD);
 
-  //Elf_Phdr *pro_head = (Elf_Phdr*)malloc(sizeof(Elf_Phdr)*elf_head -> e_phnum);
-  Elf_Phdr *pro_head=NULL;
+  Elf_Phdr *pro_head = (Elf_Phdr*)malloc(sizeof(Elf_Phdr)*elf_head -> e_phnum);
+  //Elf_Phdr *pro_head=NULL;
   ramdisk_read(pro_head,elf_head->e_phoff,ehdr.e_phentsize);
 
   for(Elf64_Phdr *p =pro_head; p<pro_head +elf_head->e_phnum; p++){
