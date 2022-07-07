75c195304ca69dd76335f11af6a9f0f2a21c4b38
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-08 01:15:10 +0800

    >  compile NEMU ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.13.0-52-generic #59~20.04.1-Ubuntu SMP Thu Jun 16 21:21:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  01:15:10 up 58 min,  1 user,  load average: 0.07, 0.10, 0.05

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -12,29 +12,28 @@
 extern size_t ramdisk_read(void *buf, size_t offset, size_t len);
 extern size_t get_ramdisk_size();
 Elf64_Ehdr ehdr = {0};
-Elf64_Phdr *phdr = NULL;
+Elf64_Phdr phdr = {0};
 static uintptr_t loader(PCB *pcb, const char *filename) {
   //TODO();
   //return 0;
   //自己加
   printf("loderok1\n");
-  ramdisk_read(&ehdr, 0, sizeof(Elf_Ehdr));
+  ramdisk_read(&ehdr, 0, sizeof(Elf64_Ehdr));
   printf("ehdr.e_phnum = %lx\n",ehdr.e_phnum);
   assert(*(uint32_t *)ehdr.e_ident == 0x464c457f);
-  phdr = (Elf_Phdr *)malloc(ehdr.e_phnum);
-  ramdisk_read(phdr, sizeof(Elf_Ehdr),ehdr.e_phnum);
+  //ramdisk_read(phdr, sizeof(Elf_Ehdr),ehdr.e_phnum);
   printf("ehdr.e_phnum = %lx\n",ehdr.e_phnum);
-  for(Elf_Phdr *p = phdr;p <  phdr + ehdr.e_phnum; p++){
-   
-    if(p->p_type == PT_LOAD){
-      ramdisk_read((void*)(p->p_vaddr), p->p_offset, p->p_filesz);
+  for(int i=0; i<ehdr.e_phnum; i++){
+    ramdisk_read(&phdr, ehdr.e_phoff +i*sizeof(phdr),sizeof(Elf64_Phdr));
+    if(phdr.p_type == PT_LOAD){
+      ramdisk_read((void*)(phdr.p_vaddr), phdr.p_offset, phdr.p_filesz);
       printf("eok2\n");
     }
-    if(p->p_memsz > p->p_filesz){
+    if(phdr.p_memsz > phdr.p_filesz){
       printf("eok3\n");
       //uint64_t length = phdr.p_paddr + phdr.p_filesz;
       //ramdisk_read((void*)(length), 0x0, phdr.p_memsz - phdr.p_filesz);
-      memset((void*)(p->p_vaddr + p->p_filesz),0,p->p_memsz - p->p_filesz);
+      memset((void*)(phdr.p_vaddr + phdr.p_filesz),0,phdr.p_memsz - phdr.p_filesz);
       printf("eok32\n");
   }
   }
