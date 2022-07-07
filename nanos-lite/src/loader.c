e5d81c7d301c9b81561b905c47e46ac02df280ed
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-07 20:11:01 +0800

    >  compile NEMU ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.13.0-52-generic #59~20.04.1-Ubuntu SMP Thu Jun 16 21:21:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:11:01 up  3:31,  1 user,  load average: 0.06, 0.10, 0.03

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -22,7 +22,8 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   printf("ehdr.e_phnum = %lx\n",ehdr.e_phnum);
   assert(*(uint32_t *)ehdr.e_ident == 0x464c457f);
   for(uint32_t i=0;i < ehdr.e_phnum; i++){
-    ramdisk_read(&phdr, ehdr.e_phoff + i*sizeof(phdr),sizeof(Elf64_Phdr));
+    //ramdisk_read(&phdr, ehdr.e_phoff + i*sizeof(phdr),sizeof(Elf64_Phdr));
+    ramdisk_read(&phdr, ehdr.e_phoff + i*sizeof(phdr),phdr.p_filesz);
     printf("for i = %d\n",i);
     if(phdr.p_type == PT_LOAD){
       ramdisk_read((void*)(phdr.p_paddr), phdr.p_offset, phdr.p_filesz);
