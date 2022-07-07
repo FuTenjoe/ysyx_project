d4586b80ef87a5875fb7dbca3600ea0f7643464c
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-07 12:57:23 +0800

    >  compile NEMU ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.13.0-52-generic #59~20.04.1-Ubuntu SMP Thu Jun 16 21:21:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:57:23 up  1:56,  1 user,  load average: 0.01, 0.03, 0.00

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -17,14 +17,15 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   //TODO();
   //return 0;
   //自己加
-  printf("loderok1\n");
+ /* printf("loderok1\n");
   ramdisk_read(&ehdr, 0, get_ramdisk_size());
   //assert(*(uint32_t *)ehdr.e_ident == 0x7f454c46);
   printf("loderok2\n");
   assert(*(uint32_t *)ehdr.e_ident == 0x464c457f);
-  for(int i = 0;i <ehdr.e_phnum; i++){
+  ramdisk_read(&phdr, sizeof(Elf64_Ehdr),sizeof(Elf64_Phdr)ehdr.e_phnum);
+  for(Elf64_Phdr i=0;;i <ehdr.e_phnum; i++){
     //ramdisk_read(&phdr, ehdr.e_phoff + i*sizeof(phdr),ehdr.e_phentsize);
-    ramdisk_read(&phdr, ehdr.e_phoff + i*sizeof(phdr),sizeof(Elf64_Phdr));
+    //ramdisk_read(&phdr, ehdr.e_phoff + i*sizeof(phdr),sizeof(Elf64_Phdr));
   if(phdr.p_type == PT_LOAD){
     ramdisk_read((void*)(phdr.p_paddr), phdr.p_offset,phdr.p_filesz);
     printf("eok2\n");
@@ -35,12 +36,25 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
     ramdisk_read((void*)(length), 0x0, phdr.p_memsz - phdr.p_filesz);
     printf("eok32\n");
   }
+  }*/
+  Elf_Ehdr *elf_head = (Elf_Ehdr*)malloc(sizeof(Elf_Ehdr));
+  ramdisk_read(elf_head,0,sizeof(Elf_Ehdr));
+  assert(*(uint32_t *)ehdr.e_ident == 0x464c457f);
+  assert(elf_head -> e_machine == PT_LOAD);
+
+  Elf_Phdr *pro_head = (Elf_Phdr*)malloc(sizeof(Elf_Phdr)*elf_head -> e_phnum);
+  ramdisk_read(pro_head,sizeof(Elf_Ehdr),sizeof(Elf_Phdr)*elf_head ->e_phnum);
+
+  for(Elf64_Phdr *p =pro_head; p<pro_head +elf_head->e_phnum; p++){
+    ramdisk_read((void*)(p->p_vaddr),p->p_offset,p->p_filesz);
+    memset((void*)(p->p_vaddr +p->p_filesz),0,p->p_memsz - p->p_filesz);
   }
-  
+
+
   printf("eok4\n");
   printf("e_entryad = 0x%lx\n",ehdr.e_entry);
-  return  ehdr.e_entry;
-  //return ehdr->e_entry;
+ // return  ehdr.e_entry;
+  return elf_head->e_entry;
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
