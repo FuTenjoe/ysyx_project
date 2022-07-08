116a04bb3be22d23187f42217d00c881f1492b4c
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-08 13:41:56 +0800

    >  compile NEMU ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.13.0-52-generic #59~20.04.1-Ubuntu SMP Thu Jun 16 21:21:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:41:56 up 47 min,  0 users,  load average: 0.14, 0.07, 0.06

--- nemu/src/isa/riscv64/system/intr.c
+++ nemu/src/isa/riscv64/system/intr.c
@@ -20,6 +20,7 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
 word_t isa_query_intr() {
   //return INTR_EMPTY;
   //自家加实现mret
-  return cpu.sr[833];
   printf("mret 函数ok");
+  return cpu.sr[833];
+  
 }
