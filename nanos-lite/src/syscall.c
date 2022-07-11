e0d16289d3e98253cbb6d22c92016120b4e1ffde
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-11 21:21:00 +0800

    >  compile NEMU ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.13.0-52-generic #59~20.04.1-Ubuntu SMP Thu Jun 16 21:21:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:21:00 up  1:47,  0 users,  load average: 0.00, 0.02, 0.00

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,10 +1,15 @@
 #include <common.h>
 #include "syscall.h"
+//自己加
+extern void yield();
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
 
   switch (a[0]) {
+    case -1:{
+      yield();break;
+      }
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 }
