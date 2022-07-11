2405be9a552e059928f40bc41f13dd26e4cb9e3d
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-11 19:49:05 +0800

    >  compile NEMU ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.13.0-52-generic #59~20.04.1-Ubuntu SMP Thu Jun 16 21:21:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:49:05 up 15 min,  0 users,  load average: 0.05, 0.08, 0.04

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -9,7 +9,7 @@ Context* __am_irq_handle(Context *c) {
   if (user_handler) {
     Event ev = {0};
     switch (c->mcause) {
-      case(11):{
+      case(10):{
         if((c->gpr[17] >=0) &&(c->gpr[17] <=19)){
           ev.event = EVENT_SYSCALL;break;
         }
