5a9968ec1c1f210a15a5bfc6f4403065d4b7effb
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-12 14:22:17 +0800

    >  compile NEMU ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.13.0-52-generic #59~20.04.1-Ubuntu SMP Thu Jun 16 21:21:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:22:17 up  2:10,  1 user,  load average: 0.14, 0.13, 0.06

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -30,12 +30,7 @@ void do_syscall(Context *c) {
     case 0:{
       halt(c->GPR1);break;   //是否指向这个宏存疑，以及每个宏代表的寄存器
     }
-    case 4:{
-      //write(c->GPR1, c->GPR2,c->GPR3);
-      //write();
-      //break;
-      panic("Unhandled syscall ID = %d", a[0]);
-    }
+    
     
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
