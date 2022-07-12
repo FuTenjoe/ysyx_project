f4c00b36050217885f8dc759b1ddc0397f1efae1
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-12 18:33:45 +0800

    >  compile NEMU ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.13.0-52-generic #59~20.04.1-Ubuntu SMP Thu Jun 16 21:21:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:33:45 up  6:15,  1 user,  load average: 0.00, 0.01, 0.00

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -23,7 +23,7 @@ void do_syscall(Context *c) {
   a[2] = c->GPR3;  //a1
   a[3] = c->GPR4;  //a2
   //a[4] = c->GPRx;
-  printf("gpr a0 = %lx",a[1]);
+  printf("gpr a0 = %lx\n",a[1]);
   switch (a[0]) {
     case 1:{
       yield();
