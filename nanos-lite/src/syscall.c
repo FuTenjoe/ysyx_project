bbd005b4f29875ec6c56902575db95db132b31ca
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-12 15:00:25 +0800

    >  compile NEMU ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.13.0-52-generic #59~20.04.1-Ubuntu SMP Thu Jun 16 21:21:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:00:25 up  2:48,  1 user,  load average: 0.03, 0.02, 0.00

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -11,7 +11,7 @@ size_t write( int  fd, char buf,size_t nbytes){
             //char out = buf;
               putch (buf+i);
           }
-          return 1;
+          return 0;
         }
         else return -1;
       };
