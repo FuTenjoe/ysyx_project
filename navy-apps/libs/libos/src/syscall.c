6719e9ba3da7214f69ff30ae2fd661df892fe1f3
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-12 16:33:28 +0800

    >  compile NEMU ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.13.0-52-generic #59~20.04.1-Ubuntu SMP Thu Jun 16 21:21:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:33:28 up  4:22,  1 user,  load average: 0.00, 0.00, 0.00

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -61,8 +61,8 @@ int _open(const char *path, int flags, mode_t mode) {
 }
 
 int _write(int fd, void *buf, size_t count) {
-  _syscall_(SYS_write, fd, buf, count);
-  return 1;
+  //_syscall_(SYS_write, fd, buf, count);
+  return 0;
 }
 
 void *_sbrk(intptr_t increment) {
