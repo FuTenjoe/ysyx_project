0efeae1029a8a7eaa8769e7312631699c7a29aad
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-12 16:33:14 +0800

    >  compile NEMU ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.13.0-52-generic #59~20.04.1-Ubuntu SMP Thu Jun 16 21:21:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:33:14 up  4:21,  1 user,  load average: 0.00, 0.00, 0.00

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -12,7 +12,7 @@ size_t write( int  fd, const void * buf,size_t count){
           }
           return 0;
         }
-        else return 0;
+        else return -1;
       };
   
 void do_syscall(Context *c) {
@@ -27,7 +27,7 @@ void do_syscall(Context *c) {
       yield();a[0] = 0;break;
       }
     case 0:{
-      halt(c->GPR2); break; //是否指向这个宏存疑，以及每个宏代表的寄存器
+      halt(a[1]); break; //是否指向这个宏存疑，以及每个宏代表的寄存器
     }
     case 4:{
       //a[0]=write(a[1],&a[2],a[3]);
