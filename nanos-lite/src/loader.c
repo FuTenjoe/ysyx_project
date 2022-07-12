4d24e31634a7a5dc5991d1f9c85b31d37f2419c0
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-12 11:02:52 +0800

    >  compile NEMU ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.13.0-52-generic #59~20.04.1-Ubuntu SMP Thu Jun 16 21:21:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:02:52 up  1:38,  1 user,  load average: 0.00, 0.03, 0.00

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -19,7 +19,6 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   //自己加
   printf("loderok1\n");
   //ramdisk_read(&ehdr, 0, sizeof(Elf_Ehdr));
-  ramdisk_read(&ehdr, 0, 10);
   printf("ehdr.e_phnum = %lx\n",ehdr.e_phnum);
   assert(*(uint32_t *)ehdr.e_ident == 0x464c457f);
   //ramdisk_read(phdr, sizeof(Elf_Ehdr),ehdr.e_phnum);
