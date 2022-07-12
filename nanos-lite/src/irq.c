35d7a1c07ae604bc15bb3a317abec7b00d6290d2
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-12 18:53:49 +0800

    >  compile NEMU ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.13.0-52-generic #59~20.04.1-Ubuntu SMP Thu Jun 16 21:21:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:53:49 up  6:35,  1 user,  load average: 0.00, 0.01, 0.00

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -3,8 +3,8 @@
 extern void do_syscall(Context *c);
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
-    case 1: printf("do evevt event ID = %d\n", e.event);break;
-    case 2: do_syscall(c);printf("do evevt event ID = %d\n", e.event);break;
+    case 1: printf("do evevt event ID = %d\n", e.event);c->mepc= c->mepc+4;break;
+    case 2: printf("do evevt event ID = %d\n", e.event);do_syscall(c);c->mepc= c->mepc+4;break;
     default: panic("Unhandled event ID = %d", e.event);break;
   }
 
