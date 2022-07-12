ce54a48c1bd668cabbdbad806c4391b8856b7a65
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-12 16:45:20 +0800

    >  compile NEMU ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.13.0-52-generic #59~20.04.1-Ubuntu SMP Thu Jun 16 21:21:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:45:20 up  4:33,  1 user,  load average: 0.00, 0.03, 0.00

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -7,8 +7,8 @@ size_t write( int  fd, const void * buf,size_t count){
         if((fd == 1) | (fd == 2)){
           for(int i=0; i < count-1; i++){
             char* buf = (char *)malloc((count)*sizeof(buf));
-            char out = *(buf+i);
-              putch (out);
+            //char out = *(buf+i);
+              putch ('o');
           }
           return 0;
         }
