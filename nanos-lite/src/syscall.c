a0b0a993c2b100b9ebaea53103590168c43f41ee
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-14 15:25:17 +0800

    >  compile NEMU ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.13.0-52-generic #59~20.04.1-Ubuntu SMP Thu Jun 16 21:21:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:25:17 up 16:49,  1 user,  load average: 0.14, 0.05, 0.01

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -55,7 +55,7 @@ void do_syscall(Context *c) {
       printf("gpr x = %lx\n",c->GPRx);
       break;
     }
-    case SYS_lseek:printf("gpr lseek a0 = %lx\n",a[0]); c->GPRx = fs_lseek((int)a[1], (int)a[2], (int)a[3]);break;
+    case SYS_lseek:printf("返回偏移量 = %lx\n",a[0]); c->GPRx = fs_lseek((int)a[1], (int)a[2], (int)a[3]);break;
     case SYS_close:{
       c->GPRx = fs_close((int)a[1]);break;
     }
