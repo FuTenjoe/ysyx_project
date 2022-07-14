4e4f23340b361b077eecb00b5395ea2a4e4c083a
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-14 22:05:54 +0800

    >  compile NEMU ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.13.0-52-generic #59~20.04.1-Ubuntu SMP Thu Jun 16 21:21:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:05:54 up 23:02,  1 user,  load average: 0.08, 0.09, 0.04

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -76,7 +76,7 @@ size_t fs_read(int fd, void *buf, size_t count){
       return -1;
 }
 size_t fs_write( int  fd, const void * buf,size_t count){
-  Log("fs_write:fd=%d,open_offset=%d,count=%d\n",open_i,open_offset,count);
+  Log("fs_write:open_i=%d,fd=%d,open_offset=%d,count=%d\n",open_i,fd,open_offset,count);
   assert(open_offset <= file_table[open_i].size);
         if((fd == 1) | (fd == 2)){
           int i;
