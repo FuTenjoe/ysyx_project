60c66a48efd3412e8d0ca61bb6df3c4df84a0f52
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-15 09:45:35 +0800

    >  compile NEMU ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.15.0-41-generic #44~20.04.1-Ubuntu SMP Fri Jun 24 13:27:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  09:45:35 up  1:10,  1 user,  load average: 0.02, 0.05, 0.07

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -87,7 +87,6 @@ size_t fs_write( int  fd, const void * buf,size_t count){
           int i;
           for(i=0; i < count; i++){
             putch(((char*)(buf))[i]);
-            
           }
           ramdisk_write(buf,file_table[open_i].open_offset,count);
           file_table[open_i].open_offset = file_table[open_i].open_offset + count;
@@ -95,17 +94,19 @@ size_t fs_write( int  fd, const void * buf,size_t count){
         }
     else return -1;
   }
-  else return -1;
-/*  else{
+  else{
     if((fd == 1) | (fd == 2)){
           int i;
-          for(i=0; i < file_table[open_i].size-open_offset; i++){
+          for(i=0; i < file_table[open_i].size-file_table[open_i].open_offset; i++){
             putch(((char*)(buf))[i]);
             //ramdisk_write(buf,open_offset,file_table[open_i].size-open_offset);
           }
+          ramdisk_write(buf,file_table[open_i].open_offset,count);
+          file_table[open_i].open_offset = file_table[open_i].open_offset + count;
+          return file_table[open_i].size-file_table[open_i].open_offset;
         }
-    return file_table[open_i].size-open_offset;
-  }*/
+    else return -1;
+  }
         
     
 };
