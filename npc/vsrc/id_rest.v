2afe2c1e57f88661138b9332d793f211769dd96c
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-20 09:54:11 +0800

    >  sim RTL ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.15.0-41-generic #44~20.04.1-Ubuntu SMP Fri Jun 24 13:27:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  09:54:11 up 19 min,  0 users,  load average: 0.00, 0.01, 0.02

--- npc/vsrc/id_rest.v
+++ npc/vsrc/id_rest.v
@@ -1,7 +1,8 @@
 `include "../vsrc/rvseed_defines.v"
 
 module id_rest (
-
+    input clk,
+    input rst_n,
     
     input      [`REG_ADDR_WIDTH-1:0] reg1_raddr, // register 1 read address
     input      [`REG_ADDR_WIDTH-1:0] reg2_raddr, // register 2 read address
@@ -15,10 +16,31 @@ module id_rest (
 //reg [63:0] buff;
 // register write
 // register 1 read
+reg write_1;
+reg write_2;
+reg write_3;
+always@(posedge clk or negedge rst_n)begin
+    if(!rst_n)begin
+        write_1 <= 1'b0;
+        write_2 <= 1'b0;
+        write_3 <= 1'b0;
+    end
+    else if(write_3 == 1'b1 & write_2 == 1'b1 & write_1 == 1'b1)begin
+        write_1 <= 1'b0;
+        write_2 <= 1'b0;
+        write_3 <= 1'b0;
+    end
+    else if(reg1_raddr == wb_reg_waddr | reg2_raddr == wb_reg_waddr)begin
+        write_1 <= write_ready;
+        write_2 <= write_1;
+        write_3 <= write_2;
+    end
+end
+
 always @(*) begin
     if(reg1_raddr == wb_reg_waddr | reg2_raddr == wb_reg_waddr)begin
-        if(write_ready == 1'b1)
-            rest_from_id = 1'b0;
+        if(write_3 == 1'b0)
+            rest_from_id = 1'b1;
         else
             rest_from_id = 1'b1;
     end
