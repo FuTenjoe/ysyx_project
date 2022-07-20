8b257b2531933db8288c283c4e104d9a1c69a245
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-20 21:55:09 +0800

    >  sim RTL ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.15.0-41-generic #44~20.04.1-Ubuntu SMP Fri Jun 24 13:27:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:55:09 up  8:55,  1 user,  load average: 0.01, 0.03, 0.00

--- npc/vsrc/id_ex_regs.v
+++ npc/vsrc/id_ex_regs.v
@@ -148,7 +148,7 @@ always@(posedge clk or negedge rst_n)begin
 			rd_flag_id_ex_o <= rd_flag_id_ex_o;
 			rd_buf_flag_id_ex_o <= rd_buf_flag_id_ex_o;
 			ena_id_ex_o <= ena_id_ex_o;
-			no_use <= no_use;
+			no_use <= 1'b0;
 		end
 		else begin
 			pc_id_ex_o<=pc_id_ex_i;
