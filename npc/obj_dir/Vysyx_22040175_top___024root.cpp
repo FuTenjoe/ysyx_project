4232ad8452417d228bca3a2413886984290aafe6
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-20 16:12:35 +0800

    >  sim RTL ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.15.0-41-generic #44~20.04.1-Ubuntu SMP Fri Jun 24 13:27:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:12:35 up  3:44,  1 user,  load average: 0.10, 0.05, 0.01

--- npc/obj_dir/Vysyx_22040175_top___024root.cpp
+++ npc/obj_dir/Vysyx_22040175_top___024root.cpp
@@ -191,7 +191,7 @@ VL_INLINE_OPT void Vysyx_22040175_top___024root___sequent__TOP__1(Vysyx_22040175
                                                   ? 0x80000000ULL
                                                   : 
                                                  (((IData)(vlSelf->ysyx_22040175_top__DOT__id_control_rest) 
-                                                   & (~ (IData)(vlSelf->ysyx_22040175_top__DOT__ex_pc_ready)))
+                                                   & (IData)(vlSelf->ysyx_22040175_top__DOT__ex_pc_ready))
                                                    ? vlSelf->ysyx_22040175_top__DOT__ex_next_pc
                                                    : 
                                                   (4ULL 
