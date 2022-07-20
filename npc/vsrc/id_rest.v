075460c9420bc7d4c46561830fb09ee0257f9085
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-20 14:13:35 +0800

    >  sim RTL ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.15.0-41-generic #44~20.04.1-Ubuntu SMP Fri Jun 24 13:27:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:13:35 up  1:46,  1 user,  load average: 0.04, 0.02, 0.00

--- npc/vsrc/id_rest.v
+++ npc/vsrc/id_rest.v
@@ -19,26 +19,38 @@ module id_rest (
 reg write_1;
 reg write_2;
 reg write_3;
+reg write_4;
+reg write_5;
+reg write_6;
 reg [63:0] wb_rest_pc;
 always@(posedge clk or negedge rst_n)begin
     if(!rst_n)begin
         write_1 <= 1'b0;
         write_2 <= 1'b0;
         write_3 <= 1'b0;
+        write_4 <= 1'b0;
+        write_5 <= 1'b0;
+        write_6 <= 1'b0;
         wb_rest_pc <= 32'h0000_0000;
     end
     else if(wb_pc != 32'h0000_0000 & wb_pc != 32'h8000_0000)begin
         wb_rest_pc <= wb_pc;
         //if( write_2 == 1'b1 & write_1 == 1'b1&write_3 == 1'b1 & wb_rest_pc != wb_pc )begin
-        if( write_2 == 1'b1 & write_1 == 1'b1&write_3 == 1'b1 )begin
+        if( write_6 == 1'b1 )begin
             write_1 <= 1'b0;
             write_2 <= 1'b0;
             write_3 <= 1'b0;   
+            write_4 <= 1'b0;
+            write_5 <= 1'b0;
+            write_6 <= 1'b0;
         end
         else if(reg1_raddr == wb_reg_waddr | reg2_raddr == wb_reg_waddr)begin
             write_1 <= write_ready;
             write_2 <= write_1;
             write_3 <= write_2;
+            write_4 <= write_3;
+            write_5 <= write_4;
+            write_6 <= write_5;
         end
     end
 end
