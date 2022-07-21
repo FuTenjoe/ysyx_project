2469ab05007ada608f44540deee4c509151bd720
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-21 14:16:46 +0800

    >  sim RTL ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.15.0-41-generic #44~20.04.1-Ubuntu SMP Fri Jun 24 13:27:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:16:46 up  2:34,  1 user,  load average: 0.06, 0.05, 0.02

--- npc/vsrc/id_control_rest.v
+++ npc/vsrc/id_control_rest.v
@@ -14,10 +14,11 @@ reg ct_rest_2;
 always@(posedge clk or negedge rst_n)begin
     if(!rst_n)begin
         ct_rest_1 <= 1'b0;
-        //ct_rest_2 <= 1'b0;
+        ct_rest_2 <= 1'b0;
     end
-    else if(ct_rest_1 != 1'b1 & control_rest == 1'b1)
+    else if(ct_rest_2 != 1'b1 & control_rest == 1'b1)
         ct_rest_1 <= control_rest;
+        ct_rest_2 <=  ct_rest_1;
     else
         ct_rest_1 <= 1'b0;
 end
@@ -25,7 +26,7 @@ end
 // register write
 // register 1 read
 always @(*) begin
-    if((branch == 1'b1 | jump == 1'b1) & ct_rest_1 == 1'b0)
+    if((branch == 1'b1 | jump == 1'b1) & ct_rest_2 == 1'b0)
         control_rest = 1'b1;
     else 
         control_rest = 1'b0;
