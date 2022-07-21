5821f7bab4a299cdeaef6c04a16daa144af57e0e
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-21 09:43:47 +0800

    >  sim RTL ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.15.0-41-generic #44~20.04.1-Ubuntu SMP Fri Jun 24 13:27:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  09:43:47 up 16 min,  1 user,  load average: 0.00, 0.02, 0.04

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -154,7 +154,7 @@ int main(int argc, char **argv, char **env) {
      //for (i=0;i<15 ; i++) {
       
     //top->rst = (i < 2);
-      top->rst = (i < 5);
+      top->rst = (i < 2);
     // dump variables into VCD file and toggle clock
     if(ebreak_flag){
       printf("ebreak: program is finished !\n");
@@ -209,15 +209,15 @@ int main(int argc, char **argv, char **env) {
       printf(" a5= 0x%08lx; Instruction is 0x%x\n",cpu_gpr[15],top->inst);
       printf(" s0= 0x%08lx; Instruction is 0x%x\n",cpu_gpr[8],top->inst);
       //init_difftest(img_size,port);
-    /*  if (a>2){
+      if (a>2){
         //if (a%4==1 & a/4>=1){
        //printf("a =%d \n",a);
-       
-         difftest_step(top->diff_pc);
-        }*/
-    /*    else{
+       if((top->diff_pc != 0) &&(top->diff_pc != cpu.pc))
+          difftest_step(top->diff_pc);
+        }
+      else if(a <= 2){
           init_difftest(img_size,port);
-        }*/
+        }
      }
      
       if(npc_state == NPC_ABORT){
