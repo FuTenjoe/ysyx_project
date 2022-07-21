7ce042fe846d844110f36331d605d7755c9df123
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-21 14:07:23 +0800

    >  sim RTL ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.15.0-41-generic #44~20.04.1-Ubuntu SMP Fri Jun 24 13:27:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:07:23 up  2:25,  1 user,  load average: 0.14, 0.08, 0.03

--- npc/vsrc/id_control_rest.v
+++ npc/vsrc/id_control_rest.v
@@ -15,10 +15,7 @@ module id_control_rest (
 // register 1 read
 always @(*) begin
     if(branch == 1'b1 | jump == 1'b1)
-        if(rest_from_id == 1'b0)
         control_rest = 1'b1;
-        else if(rest_from_id == 1'b1)
-        control_rest = rest_from_id;
     else 
         control_rest = 1'b0;
 end
