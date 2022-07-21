7c65bf73954f0f7ddd5e9772af68d561fe3cafef
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-21 18:05:19 +0800

    >  sim RTL ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.15.0-41-generic #44~20.04.1-Ubuntu SMP Fri Jun 24 13:27:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:05:19 up  6:22,  1 user,  load average: 0.04, 0.03, 0.02

--- npc/vsrc/if_id_regs.v
+++ npc/vsrc/if_id_regs.v
@@ -26,7 +26,14 @@ always@(posedge clk or negedge rst_n)begin
 		time_set_if_id_o <= 1'd0;
 		pc_no_use_if_id_o <= 1'd0;
 	end
-	else if((control_rest_if_id_i == 1'b1) & ( ex_pc_ready == 1'b0))begin
+/*	else if((control_rest_if_id_i == 1'b1) & ( ex_pc_ready == 1'b0))begin
+		pc_if_id_o<= pc_if_id_o;
+		instr_if_id_o<= instr_if_id_o;
+		ena_if_id_o <= ena_if_id_o;
+		time_set_if_id_o <= time_set_if_id_o;
+		pc_no_use_if_id_o <= pc_no_use_if_id_i;
+	end*/
+	else if(pc_no_use_if_id_i == 1'b1)begin
 		pc_if_id_o<= pc_if_id_o;
 		instr_if_id_o<= instr_if_id_o;
 		ena_if_id_o <= ena_if_id_o;
