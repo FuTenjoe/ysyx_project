1346e260c89ed545019196aaa2f0512421e11575
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-21 14:15:00 +0800

    >  sim RTL ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.15.0-41-generic #44~20.04.1-Ubuntu SMP Fri Jun 24 13:27:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:15:00 up  2:32,  1 user,  load average: 0.00, 0.03, 0.02

--- npc/vsrc/id_control_rest.v
+++ npc/vsrc/id_control_rest.v
@@ -1,19 +1,31 @@
 `include "../vsrc/rvseed_defines.v"
 
 module id_control_rest (
+    input clk,
+    input rst_n
     input   branch,     // branch flag
     input   jump,       // jump flag
     output  reg control_rest
     
    
 );
-
-
+reg ct_rest_1;
+reg ct_rest_2;
+always@(posedge clk or negedge rst_n)begin
+    if(!rst_n)begin
+        ct_rest_1 <= 1'b0;
+        //ct_rest_2 <= 1'b0;
+    end
+    else if(ct_rest_1 != 1'b1 & control_rest == 1'b1)
+        ct_rest_1 <= control_rest;
+    else
+        ct_rest_1 <= 1'b0;
+end
 //reg [63:0] buff;
 // register write
 // register 1 read
 always @(*) begin
-    if(branch == 1'b1 | jump == 1'b1)
+    if((branch == 1'b1 | jump == 1'b1) & ct_rest_1 == 1'b0)
         control_rest = 1'b1;
     else 
         control_rest = 1'b0;
