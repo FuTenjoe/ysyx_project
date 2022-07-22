c3cb9b87d762554de243c613f13d29ab391cd2cf
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-22 16:43:22 +0800

    >  sim RTL ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.15.0-41-generic #44~20.04.1-Ubuntu SMP Fri Jun 24 13:27:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:43:22 up  6:38,  0 users,  load average: 0.01, 0.03, 0.00

--- npc/vsrc/mux_dt_pipe.v
+++ npc/vsrc/mux_dt_pipe.v
@@ -19,45 +19,39 @@ module mux_dt_pipe (
     input  [63:0]ex_reg2_data
 );
 reg [2:0]test;
-always@(posedge clk or negedge rst_n)begin
-    if(!rst_n)begin
-        reg1_rdata <= 64'd0;
-        reg2_rdata <= 64'd0;
-        test <= 1'b0;
-    end
-    else begin
+always@(*)begin
       //  if(control_rest != 1'b1)begin
             if(rd_buf_flag == 3'd1|rd_buf_flag == 3'd2 |rd_buf_flag == 3'd4 |rd_buf_flag == 3'd6)begin
-                test <= 1'b0;
+                test<= 1'b0;
                 if(reg1_raddr == reg_waddr)begin
-                    reg1_rdata <= from_mem_alu_res;
-                    reg2_rdata <= reg2_rdata_fr_read;
+                    reg1_rdata = from_mem_alu_res;
+                    reg2_rdata = reg2_rdata_fr_read;
                 end
                 else if(reg2_raddr == reg_waddr)begin
-                    reg1_rdata <= reg1_rdata_fr_read;
-                    reg2_rdata <= from_mem_alu_res;
+                    reg1_rdata = reg1_rdata_fr_read;
+                    reg2_rdata = from_mem_alu_res;
                    
                 end
                 else begin
-                    reg1_rdata <= reg1_rdata_fr_read;
-                    reg2_rdata <= reg2_rdata_fr_read;
+                    reg1_rdata = reg1_rdata_fr_read;
+                    reg2_rdata = reg2_rdata_fr_read;
                 end
             end
             else begin
                 if(reg1_raddr == reg_waddr)begin
-                    reg1_rdata <= from_ex_alu_res;
-                    reg2_rdata <= reg2_rdata_fr_read;
-                    test <= 3'b1;
+                    reg1_rdata = from_ex_alu_res;
+                    reg2_rdata = reg2_rdata_fr_read;
+                    test = 3'b1;
                 end
                 else if(reg2_raddr == reg_waddr)begin
-                    reg1_rdata <= reg1_rdata_fr_read;
-                    reg2_rdata <= from_ex_alu_res;
-                    test <= 3'd2;
+                    reg1_rdata = reg1_rdata_fr_read;
+                    reg2_rdata = from_ex_alu_res;
+                    test = 3'd2;
                 end
                 else begin
-                    reg1_rdata <= reg1_rdata_fr_read;
-                    reg2_rdata <= reg2_rdata_fr_read;
-                    test <= 3'd3;
+                    reg1_rdata = reg1_rdata_fr_read;
+                    reg2_rdata = reg2_rdata_fr_read;
+                    test = 3'd3;
                 end
             end
        // end
@@ -65,7 +59,6 @@ always@(posedge clk or negedge rst_n)begin
             reg1_rdata <= reg1_rdata_fr_read;
             reg2_rdata <= reg2_rdata_fr_read;
         end*/
-    end
 end
 
 
