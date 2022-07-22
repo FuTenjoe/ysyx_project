82b204aacbe3e9e50b48f9f6230f6359c7264039
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-22 11:48:44 +0800

    >  sim RTL ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.15.0-41-generic #44~20.04.1-Ubuntu SMP Fri Jun 24 13:27:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:48:44 up  1:44,  0 users,  load average: 0.00, 0.00, 0.00

--- npc/vsrc/mux_dt_pipe.v
+++ npc/vsrc/mux_dt_pipe.v
@@ -24,7 +24,7 @@ always@(posedge clk or negedge rst_n)begin
         test <= 1'b0;
     end
     else begin
-        if(control_rest != 1'b0)begin
+        if(control_rest != 1'b1)begin
             if(rd_buf_flag == 3'd1|rd_buf_flag == 3'd2 |rd_buf_flag == 3'd4 |rd_buf_flag == 3'd6)begin
                 if(reg1_raddr == reg_waddr)begin
                     reg1_rdata <= from_mem_alu_res;
