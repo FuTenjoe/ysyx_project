d7f3871d9ce1299ac91e560297c15bf959e2f8ff
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-23 17:50:54 +0800

    >  sim RTL ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.15.0-41-generic #44~20.04.1-Ubuntu SMP Fri Jun 24 13:27:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:50:54 up 8 min,  0 users,  load average: 0.00, 0.06, 0.05

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -209,7 +209,7 @@ int main(int argc, char **argv, char **env) {
       printf(" a5= 0x%08lx; Instruction is 0x%x\n",cpu_gpr[15],top->inst);
       printf(" s0= 0x%08lx; Instruction is 0x%x\n",cpu_gpr[8],top->inst);
       //init_difftest(img_size,port);
-      if (a>1){
+      if (a>0){
         //if (a%4==1 & a/4>=1){
        //printf("a =%d \n",a);
        if((top->diff_pc != top-> diff_delay_pc)){
