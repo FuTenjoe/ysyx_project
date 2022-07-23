2d42f72e6b5a8e74a0a473be8c4477b562d93b79
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-23 14:46:02 +0800

    >  sim RTL ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.15.0-41-generic #44~20.04.1-Ubuntu SMP Fri Jun 24 13:27:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:46:02 up  3:42,  1 user,  load average: 0.02, 0.03, 0.00

--- npc/vsrc/wb_stage.v
+++ npc/vsrc/wb_stage.v
@@ -16,12 +16,12 @@ module wb_stage (
     input [3:0] expand_signed,
     input [2:0]rd_buf_flag,
     input ebreak_flag,
-    output  [63:0] reg_f [0:`REG_DATA_DEPTH-1],
-    //output write_ready,
+    output reg [63:0] reg_f [0:`REG_DATA_DEPTH-1],
+    output write_ready,
     input wb_no_use
    
 );
-wire [63:0] reg_wdata;
+reg [63:0] reg_wdata;
 always@(*)begin
     if(rd_buf_flag == 3'd1|rd_buf_flag == 3'd2 |rd_buf_flag == 3'd4 |rd_buf_flag == 3'd6 )begin
         reg_wdata = from_mem_alu_res;
@@ -30,29 +30,32 @@ always@(*)begin
         reg_wdata = from_ex_alu_res;
     end
 end
-reg [2:0]write_ready;
-always @(*) begin
+
+always @(posedge clk or negedge rst_n) begin
+    if(~rst_n)
+        write_ready <= 1'b0;
+    else begin
         if (rst_n && reg_wen && (reg_waddr != `REG_ADDR_WIDTH'b0)&&(s_flag==1'd0))begin // x0 read only
             case(expand_signed)
             4'd0:begin
-                reg_f[reg_waddr] = reg_wdata;   //jalr
-                write_ready = 3'd1;
+                reg_f[reg_waddr] <= reg_wdata;   //jalr
+                write_ready <= 1'b1;
             end
             4'd1:begin
-                reg_f[reg_waddr] = {{32{reg_wdata[31]}},reg_wdata[31:0]};   //lw  addw  divw
-                write_ready = 3'd2;
+                reg_f[reg_waddr] <= {{32{reg_wdata[31]}},reg_wdata[31:0]};   //lw  addw  divw
+                write_ready <= 1'b1;
             end
             4'd2:begin
-                reg_f[reg_waddr] = reg_wdata[31:0];            //addw错误
-                write_ready = 1'b1;
+                reg_f[reg_waddr] <= reg_wdata[31:0];            //addw错误
+                write_ready <= 1'b1;
             end
             4'd3:begin
-                reg_f[reg_waddr] = {{48{reg_wdata[15]}},reg_wdata[15:0]}; //lh
-                write_ready<= 1'b1;
+                reg_f[reg_waddr] <= {{48{reg_wdata[15]}},reg_wdata[15:0]}; //lh
+                write_ready <= 1'b1;
             end
             default:begin
-                reg_f[reg_waddr] = reg_f[reg_waddr];
-                write_ready = 1'b0;
+                reg_f[reg_waddr] <=reg_f[reg_waddr];
+                write_ready <= 1'b0;
             end
             endcase
         end
@@ -63,12 +66,13 @@ always @(*) begin
         write_ready <= write_ready;
     end*/
 
-
+end
 
 import "DPI-C" function void set_gpr_ptr(input logic [63:0] a []);
-
-    initial   set_gpr_ptr(reg_f);  // rf为通用寄存器的二维数组变量
-
+always@(*)begin
+    if(!rst_n)
+        set_gpr_ptr(reg_f);  // rf为通用寄存器的二维数组变量
+end
 import "DPI-C" function void pmem_write(input longint waddr, input longint wdata, input byte wmask);
 //wire [63:0] rdata;
 always @(*) begin
