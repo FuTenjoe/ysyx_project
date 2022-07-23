8dc7730c4f67746a01b5eb25db2a4d1fea152f59
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-23 18:19:49 +0800

    >  sim RTL ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.15.0-41-generic #44~20.04.1-Ubuntu SMP Fri Jun 24 13:27:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:19:49 up 37 min,  0 users,  load average: 0.00, 0.02, 0.00

--- npc/vsrc/id_rest.v
+++ npc/vsrc/id_rest.v
@@ -19,7 +19,7 @@ module id_rest (
 );
 
 always @(*) begin
-    if(id_pc != 32'h0000_0000 & id_pc != 32'h8000_0000 & ex_inst != 32'b0010011 )begin
+    if(id_pc != 32'h0000_0000 & id_pc != 32'h8000_0000 & reg_waddr != 1'b0 )begin
         if(rd_buf_flag == 3'd1|rd_buf_flag == 3'd2 |rd_buf_flag == 3'd4 |rd_buf_flag == 3'd6)begin
             if(reg1_raddr == reg_waddr | reg2_raddr == reg_waddr)begin
                 rest_from_id = 1'b1;
