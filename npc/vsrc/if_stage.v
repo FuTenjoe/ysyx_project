43f3be2e6dae7db2d3dffd1e14542d119ec9dfb5
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-23 16:09:10 +0800

    >  sim RTL ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.15.0-41-generic #44~20.04.1-Ubuntu SMP Fri Jun 24 13:27:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:09:10 up  5:05,  1 user,  load average: 0.01, 0.03, 0.00

--- npc/vsrc/if_stage.v
+++ npc/vsrc/if_stage.v
@@ -3,30 +3,38 @@
 module if_stage (
     input clk,
     input rst_n,
-    input      [`CPU_WIDTH-1:0] id_next_pc,
+    input      [`CPU_WIDTH-1:0] next_pc,
     output ena,
     output [31:0] inst,
     output [63:0] curr_pc,
     input control_rest,
-    input rest_id_mem
-  //  input [63:0] id_pc,
- 
+    input ex_pc_ready,
+    output reg pc_no_use,
+    input rest_from_id,
+    input [63:0] id_pc
 );
 pc_predict u_pc_predict(
-    .clk(clk),     // system clock
-    .rst_n(rst_n),   // active low reset
-    .control_rest(control_rest),
-    .id_next_pc(id_next_pc), // from id
-    .ena(ena), 
-    .curr_pc(curr_pc),  // current pc addr
-    .rest_id_mem(rest_id_mem)
+  .clk(clk),     // system clock
+  .rst_n(rst_n),   // active low reset
+  .control_rest(control_rest),
+  .ex_next_pc(next_pc), // from ex
+  .ena(ena), 
+  .curr_pc(curr_pc),  // current pc addr
+  .ex_pc_ready(ex_pc_ready),
+  .rest_from_id(rest_from_id),
+  .id_pc(id_pc),
+  .delay_rest(delay_rest)
 );
 
 import "DPI-C" function void pmem_read(input longint raddr, output longint rdata);
 //import "DPI-C" function void pmem_write(input longint waddr, input longint wdata, input byte wmask);
 wire [63:0] rdata;
 always @(*) begin
-  pmem_read(curr_pc, rdata);
+  //pmem_read(curr_pc, rdata);
+  if(control_rest == 1'b0)
+    pmem_read(curr_pc, rdata);
+  else if(ex_pc_ready == 1'b1)
+    pmem_read(curr_pc, rdata);
 end
 assign inst = rdata[31:0];
 endmodule
