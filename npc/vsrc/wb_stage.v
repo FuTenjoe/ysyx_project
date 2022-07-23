e952b7ec953a80a225ecf9023a86b02d9e977f58
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-23 17:31:49 +0800

    >  sim RTL ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.15.0-41-generic #44~20.04.1-Ubuntu SMP Fri Jun 24 13:27:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:31:49 up  6:27,  1 user,  load average: 0.03, 0.04, 0.00

--- npc/vsrc/wb_stage.v
+++ npc/vsrc/wb_stage.v
@@ -71,10 +71,10 @@ end
 
 
 import "DPI-C" function void set_gpr_ptr(input logic [63:0] a []);
-
-    initial begin
+always@(*)begin
+    (!rst_n)
         set_gpr_ptr(reg_f);  // rf为通用寄存器的二维数组变量
-    end
+end
 import "DPI-C" function void pmem_write(input longint waddr, input longint wdata, input byte wmask);
 //wire [63:0] rdata;
 always @(*) begin
