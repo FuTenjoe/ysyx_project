5c1a60b80fef1f2ba5bc0036f8ca09170631e89a
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-24 21:06:38 +0800

    >  sim RTL ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.15.0-41-generic #44~20.04.1-Ubuntu SMP Fri Jun 24 13:27:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:06:38 up  4:53,  0 users,  load average: 0.00, 0.04, 0.04

--- npc/vsrc/id_rest.v
+++ npc/vsrc/id_rest.v
@@ -24,7 +24,7 @@ module id_rest (
 always @(*) begin
     if(id_pc != 32'h0000_0000 & id_pc != 32'h8000_0000 & reg_waddr != 1'b0)begin
         if(rd_buf_flag == 3'd1|rd_buf_flag == 3'd2 |rd_buf_flag == 3'd4 |rd_buf_flag == 3'd6)begin
-            if((reg1_raddr == reg_waddr & reg2_raddr == reg_waddr) | (reg1_raddr == wb_reg_waddr & reg2_raddr == wb_reg_waddr)& (wb_reg_waddr!= 5'b0))begin
+            if((reg1_raddr == wb_reg_waddr & reg2_raddr == reg_waddr) | (reg1_raddr == reg_waddr & reg2_raddr == wb_reg_waddr)& (wb_reg_waddr!= 5'b0))begin
                 rest_from_id = 1'b1;
                 rest_id_mem = 1'b1;
                 rest_wb_hazard = 1'b1;
@@ -46,7 +46,7 @@ always @(*) begin
             end
         end
         else begin
-            if((reg1_raddr == reg_waddr & reg2_raddr == reg_waddr) | (reg1_raddr == wb_reg_waddr & reg2_raddr == wb_reg_waddr)& (wb_reg_waddr!= 5'b0))begin
+            if((reg1_raddr == reg_waddr & reg2_raddr == wb_reg_waddr) | (reg1_raddr == wb_reg_waddr & reg2_raddr == reg_waddr)& (wb_reg_waddr!= 5'b0))begin
                 rest_from_id = 1'b1;
                 rest_id_mem = 1'b1;
                 rest_wb_hazard = 1'b1;
