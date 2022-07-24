45e96cd76d53e8717b026d16ca89467045aace97
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-24 23:02:20 +0800

    >  sim RTL ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.15.0-41-generic #44~20.04.1-Ubuntu SMP Fri Jun 24 13:27:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:02:20 up  6:49,  0 users,  load average: 0.00, 0.00, 0.00

--- npc/vsrc/id_rest.v
+++ npc/vsrc/id_rest.v
@@ -32,7 +32,7 @@ end
 always @(*) begin
     if(id_pc != 32'h0000_0000 & id_pc != 32'h8000_0000 & reg_waddr != 1'b0 & delay_rest_id_mem != 1'b1)begin
         if(rd_buf_flag == 3'd1|rd_buf_flag == 3'd2 |rd_buf_flag == 3'd4 |rd_buf_flag == 3'd6)begin
-            if(((reg1_raddr == wb_reg_waddr & reg2_raddr == reg_waddr) | (reg1_raddr == reg_waddr & reg2_raddr == wb_reg_waddr))& (wb_reg_waddr!= 5'b0)&(s_imm == 32'd0))begin
+    /*       if(((reg1_raddr == wb_reg_waddr & reg2_raddr == reg_waddr) | (reg1_raddr == reg_waddr & reg2_raddr == wb_reg_waddr))& (wb_reg_waddr!= 5'b0)&(s_imm == 32'd0))begin
                 rest_from_id = 1'b1;
                 rest_id_mem = 1'b1;
                 rest_wb_hazard = 1'b1;
@@ -52,6 +52,28 @@ always @(*) begin
                 rest_from_id = 1'b0;
                 rest_wb_hazard = 1'b0;
             end
+        end*/
+        if(((reg1_raddr == reg_waddr & reg2_raddr == wb_reg_waddr) | (reg1_raddr == wb_reg_waddr & reg2_raddr == reg_waddr))& (wb_reg_waddr!= 5'b0))begin
+                rest_from_id = 1'b1;
+                rest_id_mem = 1'b0;
+                rest_wb_hazard = 1'b1;
+            end
+            else if(reg1_raddr == reg_waddr | reg2_raddr == reg_waddr)begin
+                rest_from_id = 1'b1;
+                rest_id_mem = 1'b0;
+                rest_wb_hazard = 1'b0;
+            end
+            else if((reg1_raddr == wb_reg_waddr | reg2_raddr == wb_reg_waddr) & (wb_reg_waddr!= 5'b0))begin
+                rest_from_id = 1'b1;
+                rest_id_mem = 1'b0;
+                rest_wb_hazard = 1'b1;
+            end
+            else begin
+                rest_from_id = 1'b0;
+                rest_id_mem = 1'b0;
+                rest_wb_hazard = 1'b0;
+            end
+        end
         end
         else begin
             if(((reg1_raddr == reg_waddr & reg2_raddr == wb_reg_waddr) | (reg1_raddr == wb_reg_waddr & reg2_raddr == reg_waddr))& (wb_reg_waddr!= 5'b0))begin
