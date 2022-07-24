03c4bd1a46942d16dd0bc2f6f7b224709f05e663
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-24 14:52:35 +0800

    >  sim RTL ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.15.0-41-generic #44~20.04.1-Ubuntu SMP Fri Jun 24 13:27:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:52:35 up  2:37,  0 users,  load average: 0.01, 0.01, 0.00

--- npc/vsrc/mux_dt_pipe.v
+++ npc/vsrc/mux_dt_pipe.v
@@ -18,10 +18,12 @@ module mux_dt_pipe (
     input s_flag,
     output reg [63:0] end_write_addr,
     input [63:0] wb_hazard_result,
-    input  [`REG_ADDR_WIDTH-1:0]  wb_reg_waddr
+    input  [`REG_ADDR_WIDTH-1:0]  mem_reg_waddr
 );
 reg [2:0]test;
-always@(*)begin
+reg [2:0] data_rest_cond;
+
+/*always@(*)begin
      reg1_rdata = reg_f[reg1_raddr];
      reg2_rdata = reg_f[reg2_raddr];
     if(rest_from_id == 1'b1)begin
@@ -72,6 +74,61 @@ always@(*)begin
             reg2_rdata = reg_f[reg2_raddr];
             test = 3'd0;
         end
+end*/
+
+
+always@(*)begin
+    data_rest_cond = {rest_from_id,rest_id_mem,rest_wb_hazard};
+    case(data_rest_cond)
+        3'b000:begin
+            reg1_rdata = reg_f[reg1_raddr];
+            reg2_rdata = reg_f[reg2_raddr];
+        end
+        3'b100:begin
+            if(reg1_raddr == reg_waddr)begin
+                reg1_rdata = from_ex_alu_res;
+                reg2_rdata = reg_f[reg2_raddr];
+            end
+            else if(reg2_raddr == reg_waddr)begin
+                reg1_rdata = reg_f[reg1_raddr];
+                reg2_rdata = from_ex_alu_res;
+            end
+        end
+        3'b110:begin
+            if(reg1_raddr == reg_waddr)begin
+                reg1_rdata = from_mem_alu_res;
+                reg2_rdata = reg_f[reg2_raddr];
+            end
+            else if(reg2_raddr == reg_waddr)begin
+                reg1_rdata = reg_f[reg1_raddr];
+                reg2_rdata = from_mem_alu_res;
+            end
+        end
+        3'b101:begin
+            if(reg1_raddr == mem_reg_waddr)begin
+                reg1_rdata = wb_hazard_result;
+                reg2_rdata = reg_f[reg2_raddr];
+            end
+            else if(reg2_raddr == reg_waddr)begin
+                reg1_rdata = reg_f[reg1_raddr];
+                reg2_rdata = wb_hazard_result;
+            end
+        end
+        3'b111:begin
+            if((reg1_raddr == mem_reg_waddr) & (reg2_raddr == reg_waddr))begin
+                reg1_rdata = wb_hazard_result;
+                reg2_rdata = from_mem_alu_res;
+            end
+            else if((reg1_raddr == reg_waddr) & (reg2_raddr == mem_reg_waddr))begin
+                reg1_rdata = from_mem_alu_res;
+                reg2_rdata = wb_hazard_result;
+            end
+        end
+        default:begin
+             reg1_rdata = reg_f[reg1_raddr];
+            reg2_rdata = reg_f[reg2_raddr];
+        end
+    endcase
 end
 
 always@(*)begin
