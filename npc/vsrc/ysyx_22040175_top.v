d0978f8d18e33aea284f5eee0b65eec7d423c302
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-24 22:50:24 +0800

    >  sim RTL ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.15.0-41-generic #44~20.04.1-Ubuntu SMP Fri Jun 24 13:27:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:50:24 up  6:37,  0 users,  load average: 0.11, 0.07, 0.02

--- npc/vsrc/ysyx_22040175_top.v
+++ npc/vsrc/ysyx_22040175_top.v
@@ -125,8 +125,7 @@ id_stage u_id_stage(
     .ex_inst(ex_inst),
     .end_write_addr(id_end_write_addr),
     .wb_hazard_result(wb_hazard_result),
-    .mem_reg_waddr(mem_reg_waddr),
-    .mem_s_imm(mem_s_imm)
+    .mem_reg_waddr(mem_reg_waddr)
 );
 wire [63:0] ex_pc;
 wire        ex_branch;     // branch flag
