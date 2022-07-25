76cb4ac0afa532a79c9bbab804e60009b99db24d
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-25 11:27:24 +0800

    >  sim RTL ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.15.0-41-generic #44~20.04.1-Ubuntu SMP Fri Jun 24 13:27:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:27:24 up  2:09,  1 user,  load average: 0.00, 0.00, 0.00

--- npc/vsrc/ctrl.v
+++ npc/vsrc/ctrl.v
@@ -701,7 +701,7 @@ always @(*) begin
 
                 //后加
                 reg1_raddr = rs1;
-                reg2_raddr = rs2;
+                reg2_raddr = rs1;
                 end
                 default:unknown_code = inst;
             endcase
