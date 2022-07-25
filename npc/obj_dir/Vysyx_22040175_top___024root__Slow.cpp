a99e1c9021d1716c8ca96598dd71bef2daa1d6d0
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-25 22:54:29 +0800

    >  compile NEMU ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.15.0-41-generic #44~20.04.1-Ubuntu SMP Fri Jun 24 13:27:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:54:29 up 11:19,  1 user,  load average: 0.16, 0.03, 0.01

--- npc/obj_dir/Vysyx_22040175_top___024root__Slow.cpp
+++ npc/obj_dir/Vysyx_22040175_top___024root__Slow.cpp
@@ -2572,7 +2572,7 @@ void Vysyx_22040175_top___024root___settle__TOP__2(Vysyx_22040175_top___024root*
                          | (1U == (0x7fU & (IData)(
                                                    ((QData)((IData)(vlSelf->ysyx_22040175_top__DOT__id_inst)) 
                                                     >> 0x19U)))))) {
-                        vlSelf->ysyx_22040175_top__DOT__u_id_stage__DOT__imm_gen_op = 5U;
+                        vlSelf->ysyx_22040175_top__DOT__u_id_stage__DOT__imm_gen_op = 0U;
                     }
                 }
             }
