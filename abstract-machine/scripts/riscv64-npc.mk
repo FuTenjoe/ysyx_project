7b66579c4aaa44b8acb1d7ccd3a7ba0af3af54d0
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-25 23:16:35 +0800

    >  sim RTL ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.15.0-41-generic #44~20.04.1-Ubuntu SMP Fri Jun 24 13:27:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:16:35 up 11:41,  1 user,  load average: 0.04, 0.08, 0.03

--- abstract-machine/scripts/riscv64-npc.mk
+++ abstract-machine/scripts/riscv64-npc.mk
@@ -13,7 +13,7 @@ AM_SRCS := riscv/npc/start.S \
 CFLAGS    += -fdata-sections -ffunction-sections
 LDFLAGS   += -T $(AM_HOME)/scripts/linker.ld --defsym=_pmem_start=0x80000000 --defsym=_entry_offset=0x0
 LDFLAGS   += --gc-sections -e _start
-NPCFLAGS += -l $(shell dirname $(IMAGE).elf)/npc-log.txt 
+NPCFLAGS += -b -l $(shell dirname $(IMAGE).elf)/npc-log.txt 
 
 CFLAGS += -DMAINARGS=\"$(mainargs)\"
 .PHONY: $(AM_HOME)/am/src/riscv/npc/trm.c
