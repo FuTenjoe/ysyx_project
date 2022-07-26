7c448483b87f9ef9c45da93b6d333085de18a5f2
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-26 16:04:47 +0800

    >  compile NEMU ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.15.0-41-generic #44~20.04.1-Ubuntu SMP Fri Jun 24 13:27:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:04:47 up  4:32,  1 user,  load average: 0.00, 0.01, 0.00

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -85,7 +85,7 @@ size_t fs_write( int  fd, const void * buf,size_t count){
           }
           //ramdisk_write(buf,file_table[open_i].open_offset,count);
           //file_table[open_i].open_offset = file_table[open_i].open_offset + count;
-           return count-i;
+           return i;
         }
     else return -1;
   }
