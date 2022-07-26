3417c313eb1c6a1b0c8d7746dd869fff54b21cbe
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-26 15:36:39 +0800

    >  compile NEMU ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.15.0-41-generic #44~20.04.1-Ubuntu SMP Fri Jun 24 13:27:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:36:39 up  4:04,  1 user,  load average: 0.06, 0.04, 0.05

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -34,13 +34,11 @@ static Finfo file_table[] __attribute__((used)) = {
 //自己加
 extern size_t ramdisk_read(void *buf, size_t offset, size_t len);
 extern size_t ramdisk_write(const void *buf, size_t offset, size_t len);
-int lseek_offset = 0;
 int open_i = 0;
 int fs_open(char* pathname, int flags, size_t mode){
   for(int i=0; ;i++){
       assert(file_table[i].name!=NULL);
     if(strcmp(pathname,file_table[i].name)==0){
-      //printf("fs_open success!");
       open_i = i;
       return i;
     }
@@ -54,30 +52,27 @@ size_t fs_offset(int fd){
   return file_table[fd].disk_offset;  //为了调整loader中的偏移量
 }
 int fs_lseek(int fd, int offset, int whence){
-  int ret = 0;
   switch (whence)
   {
-  case SEEK_SET: ret = offset;break;
+  case SEEK_SET: file_table[fd].open_offset = offset;break;
   case SEEK_CUR: {
     assert(offset <= file_table[fd].size);
-    ret = ret + offset;break;
+    file_table[fd].open_offset = file_table[fd].open_offset + offset;break;
   }
-  case SEEK_END: ret = file_table[fd].size + offset;break;
-  default: ret = -1;
+  case SEEK_END: file_table[fd].open_offset = file_table[fd].size + file_table[fd].open_offset;break;
+  default: file_table[fd].open_offset = -1;
   }
-  printf("fs_lssek ret = %d\n",ret);
-  file_table[fd].open_offset = ret;
-  return ret;
+  printf("fs_lssek ret = %d\n",file_table[fd].open_offset);
+  
+  return file_table[fd].open_offset;
 }
 size_t fs_read(int fd, void *buf, size_t count){
-   Log("fs_read:fd=%d,lseek_offset=%d ,count=%d\n",fd,lseek_offset,count);
+   Log("fs_read:fd=%d, ,count=%d\n",fd,count);
    //if(lseek_offset <= file_table[fd].size)
    size_t ret;
-      ret =  ramdisk_read(buf, file_table[fd].open_offset+lseek_offset, count);
+      ret =  ramdisk_read(buf, file_table[fd].open_offset, count);
       file_table[fd].open_offset = file_table[fd].open_offset + count;
       return ret;
-      //return ramdisk_read(buf, file_table[open_i].disk_offset+open_offset, count);
-  
 }
 size_t fs_write( int  fd, const void * buf,size_t count){
   Log("fs_write:open_i=%d,fd=%d,open_offset=%d,count=%d\n",open_i,fd,file_table[open_i].open_offset,count);
