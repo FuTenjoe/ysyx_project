f1d27d265995dad086e5572eae8d485f39dc47e2
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-27 10:07:28 +0800

    >  sim RTL ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.15.0-41-generic #44~20.04.1-Ubuntu SMP Fri Jun 24 13:27:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:07:28 up 38 min,  0 users,  load average: 0.08, 0.04, 0.02

--- npc/vsrc/alu.v
+++ npc/vsrc/alu.v
@@ -17,8 +17,8 @@ reg [63:0] alu_res;
 reg signed [63:0] signed_alu_src1;
 reg signed [63:0] signed_alu_src2;
 always @(*) begin
+    zero = 1'b0;
     case (alu_op)
-        zero = 1'b0;
         `ALU_ADD: begin  //0011
         if(rd_flag == 3'd0)begin
             alu_res = (alu_src1 +  alu_src2);
