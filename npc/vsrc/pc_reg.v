a01a7bf42ad54d2d7a6d5115b8e7e27255b815da
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-27 10:42:41 +0800

    >  sim RTL ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.15.0-41-generic #44~20.04.1-Ubuntu SMP Fri Jun 24 13:27:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:42:41 up  1:13,  1 user,  load average: 0.28, 0.12, 0.03

---
+++ npc/vsrc/pc_reg.v
@@ -0,0 +1,25 @@
+`include "../vsrc/rvseed_defines.v"
+
+module pc_reg (
+    input                       clk,     // system clock
+    input                       rst_n,   // active low reset
+    output reg                  ena,     // system enable
+    input      [`CPU_WIDTH-1:0] next_pc, // next pc addr
+    output reg [`CPU_WIDTH-1:0] curr_pc  // current pc addr
+);
+
+always @ (posedge clk or negedge rst_n) begin
+    if(~rst_n)
+        ena <= 1'b0;
+    else
+        ena <= 1'b1;      
+end
+
+always @ (posedge clk or negedge rst_n) begin
+    if(~rst_n)
+        curr_pc <= 32'h8000_0000;
+    else
+        curr_pc <= next_pc;
+end    
+
+endmodule
