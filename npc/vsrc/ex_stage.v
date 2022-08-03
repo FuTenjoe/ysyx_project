3bce8d67699a7cb3c50b40e5dec809bf137cab26
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-03 20:04:45 +0800

    >  sim RTL ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.15.0-41-generic #44~20.04.1-Ubuntu SMP Fri Jun 24 13:27:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:04:45 up  6:36,  1 user,  load average: 0.12, 0.17, 0.10

--- npc/vsrc/ex_stage.v
+++ npc/vsrc/ex_stage.v
@@ -1,6 +1,8 @@
 `include "../vsrc/rvseed_defines.v"
 
 module ex_stage(
+    input clk,
+    input rst_n,
     input      [`ALU_OP_WIDTH-1:0] alu_op,   // alu opcode
     input      [63:0]    alu_src1, // alu source 1
     //input      [`CPU_WIDTH-1:0]    alu_src2, // alu source 2
@@ -8,11 +10,15 @@ module ex_stage(
    // output reg                     zero,     // alu result is zero
     output reg [63:0]    alu_res,   // alu result
     input [2:0]rd_flag,
-    input [3:0] expand_signed
+    input [3:0] expand_signed,
+    output sh_fnsh_flag
+    
 );
 
 
 alu u_alu(
+    .clk(clk),
+    .rst_n(rst_n),
     .alu_op(alu_op),   // alu opcode
     .alu_src1(alu_src1), // alu source 1
     //input      [`CPU_WIDTH-1:0]    alu_src2, // alu source 2
@@ -20,7 +26,8 @@ alu u_alu(
     //.zero(zero),     // alu result is zero
     .alu_res_ex_sign(alu_res),   // alu result
     .rd_flag(rd_flag),
-    .expand_signed(expand_signed)
+    .expand_signed(expand_signed),
+    .sh_fnsh_flag(sh_fnsh_flag)
 );
 
 
