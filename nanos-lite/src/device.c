358f24f6a7222d646390dd013be6b08f6c17828c
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-05 21:48:14 +0800

    >  compile NEMU ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.15.0-43-generic #46~20.04.1-Ubuntu SMP Thu Jul 14 15:20:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:48:14 up  8:41,  1 user,  load average: 0.11, 0.09, 0.08

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -46,10 +46,11 @@ size_t events_read(void *buf, size_t offset, size_t len) {
   }
   else{
     //len = sprintf(buf,"t %u\n",io_read(AM_TIMER_UPTIME).us);
+     printf("else condition \n");
     len = 0;
   }
   //return len;
-  return 12;
+  return len;
   //参考代码
  /* AM_INPUT_KEYBRD_T ev = io_read(AM_INPUT_KEYBRD);
   if(ev.keycode == AM_KEY_NONE){
