c7056bb4060cf6a9b4488e3d444f3cdbef5b4d93
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-05 20:54:10 +0800

    >  compile NEMU ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.15.0-43-generic #46~20.04.1-Ubuntu SMP Thu Jul 14 15:20:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:54:10 up  7:47,  1 user,  load average: 0.14, 0.15, 0.05

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -53,13 +53,13 @@ void do_syscall(Context *c) {
       break;
       }
     case SYS_open:{
-      printf("在进程中打开");
+      printf("在进程中打开\n");
       c->GPRx= fs_open((char *)a[1], (int)a[2], (size_t)a[3]);
       //printf("pathname is %d",(char *)a[1]);
       break;
     }
     case SYS_read:{
-      printf("读");
+      printf("读\n");
       c->GPRx=fs_read((int)a[1],(void *)a[2],(size_t)a[3]);
       //printf("gpr x = %lx\n",c->GPRx);
       //c->GPRx= 0;
