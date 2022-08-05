3c149c11aa0808ef4e282877070f3d02dad63d7e
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-05 20:43:06 +0800

    >  compile NEMU ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.15.0-43-generic #46~20.04.1-Ubuntu SMP Thu Jul 14 15:20:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:43:06 up  7:36,  1 user,  load average: 0.02, 0.03, 0.00

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -77,6 +77,7 @@ size_t fb_write(const void *buf, size_t offset, size_t len) {
 }
 int sys_gettimeofday(struct timeval *tz,struct timezone *tv)
 {
+  printf("time \n");
   tz->tv_sec = io_read(AM_TIMER_UPTIME).us /1000000;
   tz->tv_usec = io_read(AM_TIMER_UPTIME).us%1000000;
   return 0;
