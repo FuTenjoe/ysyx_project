24709fc8a45d665aa7164b0a58af65c85353eec7
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-05 23:21:46 +0800

    >  compile NEMU ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.15.0-43-generic #46~20.04.1-Ubuntu SMP Thu Jul 14 15:20:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:21:46 up 10:15,  1 user,  load average: 0.10, 0.29, 0.25

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -31,24 +31,23 @@ size_t serial_write(const void *buf, size_t offset, size_t len) {
 size_t events_read(void *buf, size_t offset, size_t len) {
   //return 0;
   //自己加
- /* int key = io_read(AM_INPUT_KEYBRD).keycode;
+  int key = io_read(AM_INPUT_KEYBRD).keycode;
   bool key_down = io_read(AM_INPUT_KEYBRD).keydown;
   if(key == AM_KEY_NONE){
     return 0;
   }
   else {
-  if(key != AM_KEY_NONE){
-    if(key_down)
+    if(key_down){
       len = sprintf((char*)buf,"kd %s\n",keyname[key]);  //按下按键
+    }
       //len = sprintf(buf,"t %u\n",io_read(AM_TIMER_UPTIME).us);
     else
-      len = sprintf((char*)buf,"ku %s\n",keyname[key]);  //松开按键
+      {len = sprintf((char*)buf,"ku %s\n",keyname[key]); } //松开按键
       //len = sprintf(buf,"t %u\n",io_read(AM_TIMER_UPTIME).us);
+      return len;
   }
-  }
-  return len;*/
   //参考代码
-  AM_INPUT_KEYBRD_T ev = io_read(AM_INPUT_KEYBRD);
+ /* AM_INPUT_KEYBRD_T ev = io_read(AM_INPUT_KEYBRD);
   if(ev.keycode == AM_KEY_NONE){
     return 0;
   }
@@ -59,13 +58,13 @@ size_t events_read(void *buf, size_t offset, size_t len) {
     }
     else len = sprintf((char*)buf,"ku%s\n",keyname[ev.keycode]);
     //if(ev.keycode <= AM_KEY_PAGEDOWN && ev.keycode >= AM_KEY_ESCAPE && ev.keydown){
-    /*if(ev.keydown){
+    if(ev.keydown){
         //len = sprintf(buf,"t %u\n",io_read(AM_TIMER_UPTIME).us);
         len = sprintf((char*)buf,"kd%s\n",keyname[ev.keycode]);
-    }*/
+    }
     //else len = sprintf((char*)buf,"ku%s\n",keyname[ev.keycode]);
     return len;
-  }
+  }*/
   //return 0;
 }
 
