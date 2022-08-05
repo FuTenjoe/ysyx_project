17a5d213618bff08a6450d4b0e5587cb614a9991
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-05 20:55:31 +0800

    >  compile NEMU ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.15.0-43-generic #46~20.04.1-Ubuntu SMP Thu Jul 14 15:20:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:55:31 up  7:48,  1 user,  load average: 0.13, 0.17, 0.07

--- navy-apps/tests/event-test/main.c
+++ navy-apps/tests/event-test/main.c
@@ -5,11 +5,11 @@ int main() {
   printf("keyboard0\n");
   NDL_Init(0);
   printf("keyboard\n");
-  while (1) {
+/*  while (1) {
     char buf[64];
     if (NDL_PollEvent(buf, sizeof(buf))) {
       printf("receive event: %s\n", buf);
     }
-  }
+  }*/
   return 0;
 }
