d4e9a08a981d905f24967003f486b5a2e7fedb2f
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-05 21:24:54 +0800

    >  compile NEMU ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.15.0-43-generic #46~20.04.1-Ubuntu SMP Thu Jul 14 15:20:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:24:54 up  8:18,  1 user,  load average: 0.17, 0.10, 0.09

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -32,10 +32,11 @@ int NDL_PollEvent(char *buf, int len) {
   fscanf(fp,"%s",buf+3); */
   //printf("%d\n",len);
   //int ret = fread(buf,1,len,fb);
-    printf("读取buf");
   evt_fd = open("/dev/events", O_RDONLY);
   int ret = read(evt_fd,buf,len);
-  if(ret == 0) return 0;
+  if(ret == 0) {
+    printf("ret = 0\n");
+    return 0;}
   printf("读取buf");
   for(int i = 0; i < len&&ret != 0;i++)
   {
