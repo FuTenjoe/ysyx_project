f85e492cae7f8bd6a21a5d1e103ed60e5b8f2bdf
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-06 16:48:29 +0800

    >  compile NEMU ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.15.0-43-generic #46~20.04.1-Ubuntu SMP Thu Jul 14 15:20:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:48:29 up  2:25,  1 user,  load average: 0.00, 0.08, 0.08

--- navy-apps/tests/bmp-test/main.cpp
+++ navy-apps/tests/bmp-test/main.cpp
@@ -4,36 +4,12 @@
 #include <BMP.h>
 #include <NDL.h>
 #include <BMP.h>
-
+extern void* BMP_Load(const char *filename, int *width, int *height);
 int main() {
-  //NDL_Init(0);
-  //int w, h;
-  //printf("1\n");
-  //void *bmp = BMP_Load("/share/pictures/projectn.bmp", &w, &h);
-  //printf("w = %d h = %d",w,h);
-  //assert(bmp);
-  //printf("2\n");
-  //NDL_OpenCanvas(&w, &h);
-  //printf("1\n");
-  /* for(int i = 0;i < w*h;i++)
-  {
-    if(((uint32_t*)bmp)[i]&0xff000000) return 0;
-    printf("p = %d %x\n",i,((uint32_t*)bmp)[i]);
-  } */
-  //printf("Finish first\n");
-  //NDL_DrawRect((uint32_t*)bmp, 0, 0, w, h);
-  //free(bmp);
-  //NDL_Quit();
-  //printf("Test ends! Spinning...\n");
-  //while (1);
-  //return 0;
-
-
-
-
   NDL_Init(0);
   int w, h;
-  void *bmp = BMP_Load("/share/pictures/projectn.bmp", &w, &h);
+  //void *bmp = BMP_Load("/share/pictures/projectn.bmp", &w, &h);
+  void *bmp = BMP_Load("/bin/bmp-test.bmp", &w, &h);
   assert(bmp);
   NDL_OpenCanvas(&w, &h);
   NDL_DrawRect((uint32_t *)bmp, 0, 0, w, h);
