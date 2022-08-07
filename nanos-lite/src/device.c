1c269daf8af6d3fe03b41957d15f6999729b80e8
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-07 20:39:05 +0800

    >  compile NEMU ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.15.0-43-generic #46~20.04.1-Ubuntu SMP Thu Jul 14 15:20:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:39:05 up  3:23,  1 user,  load average: 0.02, 0.07, 0.06

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -57,6 +57,7 @@ size_t dispinfo_read(void *buf, size_t offset, size_t len) {
   //memcpy(buf, &ramdisk_start + offset, len);
   int w = io_read(AM_GPU_CONFIG).width;
   int h = io_read(AM_GPU_CONFIG).height;
+  printf("w=%d, h=%d",w,h);
   return sprintf((char*)buf,"WIDTH:%d\nHEIGHT:%d\n",w,h);
 }
 
