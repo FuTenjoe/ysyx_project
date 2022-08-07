67268e80ee6c4264eca3dfd3363db94f86b421ad
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-07 22:37:53 +0800

    >  compile NEMU ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.15.0-43-generic #46~20.04.1-Ubuntu SMP Thu Jul 14 15:20:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:37:53 up  5:22,  1 user,  load average: 0.02, 0.03, 0.08

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -16,26 +16,13 @@ static FILE* fb = NULL, *fb_event = NULL,*fb_sync = NULL,*fb_dispinfo = NULL;
 static int canvas_w = 0, canvas_h = 0;//记录打开的画布的大小
 static uint32_t* canvas =NULL;
 static int place_x = 0,place_y = 0;
-//自己加
-//static struct timeval now;
-//#include "nanos-lite/include/fs.h"
-//extern size_t fs_read(int fd, void *buf, size_t count);
-//extern int fs_open(char* pathname, int flags, size_t mode);
 
 uint32_t NDL_GetTicks() {
- /* sys_gettimeofday(&now,NULL);
-  //printf("sec %d ms%d\n",now.tv_sec,now.tv_usec/1000);
-  return now.tv_sec*1000+now.tv_usec/1000;*/
+
   return 0;
 }
 //自己加
 int NDL_PollEvent(char *buf, int len) {
-  //printf("读取键盘");
-  
-  /* int ret = fread(buf ,1,3,fp);
-  fscanf(fp,"%s",buf+3); */
-  //printf("%d\n",len);
-  //int ret = fread(buf,1,len,fb);
   evt_fd = open("/dev/events", O_RDONLY);
   //printf("evt_fd = %d\n",evt_fd);
   // /dev/events
@@ -58,6 +45,10 @@ int NDL_PollEvent(char *buf, int len) {
 
 void NDL_OpenCanvas(int *w, int *h) {
   //自己加
+  canvas_w = w;
+  canvas_h = h;
+  canvas = malloc(sizeof(uint32_t) * w * h);
+  assert(canvas);
   canvas = (uint32_t*)malloc(sizeof(uint32_t)*(*w)*(*h));
   memset(canvas,0,sizeof(canvas));
   //原有代码
@@ -81,18 +72,14 @@ void NDL_OpenCanvas(int *w, int *h) {
 }
 void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
  //参考代码
- /*int cp_bytes = (w < (128 - x)) ? w : 128-x;
-  for(int j=0; j<32&&j<h;j++){
-    for(int i=0;i<cp_bytes;i++){
-      canvas[(y+j)*32+x+i] = *(pixels + i);;
+ for (int i = 0; i < h; i ++) {
+      printf("\033[X%d;%d", x, y + i);
+      for (int j = 0; j < w; j ++) {
+        putchar(';');
+        fwrite(&pixels[i * w + j], 1, 4, stdout);
+      }
+      printf("d\n");
     }
-    pixels = pixels + cp_bytes;
-  }
-  for (int j=0; j<32; j++){
-    fwrite((void*)(canvas+ j *w ),1,4*w,fb);
-  }*/
-
-
 }
 
 void NDL_OpenAudio(int freq, int channels, int samples) {
