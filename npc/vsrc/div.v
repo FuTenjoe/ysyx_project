14920e590e04aaacf842a85eecbc45be0073eef1
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-08 16:52:25 +0800

    >  sim RTL ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.15.0-43-generic #46~20.04.1-Ubuntu SMP Thu Jul 14 15:20:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:52:25 up  4:19,  1 user,  load average: 0.04, 0.14, 0.15

--- npc/vsrc/div.v
+++ npc/vsrc/div.v
@@ -83,20 +83,15 @@ always@(posedge clk or negedge rstn)begin
         redy2 <= 1'b0;
         redy3 <= 1'b0;
     end
-    else if(redy1 == 1'b1 & redy2 == 1'b1)begin
+    else if(redy1 == 1'b1 )begin
         redy1 <= 1'b0;
         redy2 <= 1'b0;
         redy3 <= 1'b0;
     end
-    else if(div_valid)begin
+    else begin
         redy1 <= rdy_t[0];
         redy2 <= redy1;
         redy3 <= redy2;
-    end
-    else begin
-        redy1 <= 1'b0;
-        redy2 <= 1'b0;
-        redy3 <= 1'b0;
     end
 end
 
