134857bc2b4ef842fff4e02a872df95a5e26cd6e
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-09 14:58:29 +0800

    >  compile NEMU ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.15.0-43-generic #46~20.04.1-Ubuntu SMP Thu Jul 14 15:20:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:58:29 up 13:01,  1 user,  load average: 0.07, 0.07, 0.06

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -70,7 +70,10 @@ size_t fb_write(const void *buf, size_t offset, size_t len) {
   //printf("offset = %d",offset);
   int x = (offset)%w;
   int y = (offset)/w;
-  if(offset+len > w*h) len = w*h - offset;
+  if(offset+len > w*h) {
+    len = w*h - offset;
+    printf("offset+len > w*h");
+    }
   else len = w*h;
   io_write(AM_GPU_FBDRAW,x,y,(uint32_t*)buf,128,128,true);
   //io_write(AM_GPU_FBDRAW, 0, 0, NULL, 0, 0, true);
