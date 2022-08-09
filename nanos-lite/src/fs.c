6c562aac965a5351892133bba29423748e52e354
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-09 18:16:06 +0800

    >  compile NEMU ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.15.0-43-generic #46~20.04.1-Ubuntu SMP Thu Jul 14 15:20:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:16:06 up 16:18,  1 user,  load average: 0.11, 0.23, 0.32

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -125,7 +125,7 @@ size_t fs_write( int  fd, const void * buf,size_t count){
     case FD_FB:{
 
       f ->size = fb_write(buf,f->open_offset,bytes_to_write);
-      f->open_offset = f->open_offset + 128;
+      f->open_offset = f->open_offset + 400;
       printf("f->open_offset = %d",f->open_offset);
     }
     default:
