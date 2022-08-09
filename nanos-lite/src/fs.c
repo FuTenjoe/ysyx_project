4f63f85dbc4bb75525777253fe8364c868647a10
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-09 18:01:31 +0800

    >  compile NEMU ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.15.0-43-generic #46~20.04.1-Ubuntu SMP Thu Jul 14 15:20:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:01:31 up 16:04,  1 user,  load average: 1.52, 0.64, 0.25

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -137,11 +137,11 @@ size_t fs_write( int  fd, const void * buf,size_t count){
 
 void init_fs() {
   // TODO: initialize the size of /dev/fb
- // int w = io_read(AM_GPU_CONFIG).width;
-  //int h = io_read(AM_GPU_CONFIG).height;
-  int w = 120;
-  int h = 120;
+  int w = io_read(AM_GPU_CONFIG).width;
+  int h = io_read(AM_GPU_CONFIG).height;
+  //int w = 120;
+  //int h = 120;
   file_table[FD_FB].size = w*h*4;    //参考，为什么是4？
-  //printf("size = %d",sizeof(uint32_t));
+  printf("sizew = %d",w);
   //assert(file_table[FD_SYNC].write != NULL);
 }
