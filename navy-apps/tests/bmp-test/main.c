8dad1a0659b2a75be5dc56d9a595b3462226c60a
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-09 21:36:55 +0800

    >  compile NEMU ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.15.0-43-generic #46~20.04.1-Ubuntu SMP Thu Jul 14 15:20:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:36:55 up 19:07,  1 user,  load average: 0.13, 0.33, 0.31

--- navy-apps/tests/bmp-test/main.c
+++ navy-apps/tests/bmp-test/main.c
@@ -3,16 +3,17 @@
 #include <stdlib.h>
 #include <BMP.h>
 #include <NDL.h>
-#include <BMP.h>
+
 //extern void* BMP_Load(const char *filename, int *width, int *height);
 int main() {
   NDL_Init(0);
   int w, h;
-  void *bmp = BMP_Load("/share/pictures/projectn.bmp", &w, &h);
+  //void *bmp = BMP_Load("/share/pictures/projectn.bmp", &w, &h);
+  void *bmp = BMP_Load("/share/slides/slides-0.bmp", &w, &h);
   assert(bmp);
   NDL_OpenCanvas(&w, &h);
-  //NDL_DrawRect((uint32_t *)bmp, 0, 0, w, h);
-  //free(bmp);
+  NDL_DrawRect((uint32_t *)bmp, 0, 0, w, h);
+  free(bmp);
   NDL_Quit();
   printf("Test ends! Spinning...\n");
   while (1);
