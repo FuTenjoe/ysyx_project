dc051f407acf598c65cdc87dc69f0bdc6671458e
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-09 22:52:09 +0800

    >  compile NEMU ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.15.0-43-generic #46~20.04.1-Ubuntu SMP Thu Jul 14 15:20:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:52:09 up 20:22,  1 user,  load average: 0.37, 0.26, 0.17

--- navy-apps/tests/bmp-test/main.c
+++ navy-apps/tests/bmp-test/main.c
@@ -11,7 +11,7 @@ int main() {
   //void *bmp = BMP_Load("/share/pictures/projectn.bmp", &w, &h);
   void *bmp = BMP_Load("/share/slides/slides-0.bmp", &w, &h);
   assert(bmp);
- // NDL_OpenCanvas(&w, &h);
+  NDL_OpenCanvas(&w, &h);
   NDL_DrawRect((uint32_t *)bmp, 0, 0, w, h);
   free(bmp);
   NDL_Quit();
