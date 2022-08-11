13c6357d0524ca7a6f0775db246a1459efe105e1
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-11 14:12:31 +0800

    >  compile NEMU ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.15.0-43-generic #46~20.04.1-Ubuntu SMP Thu Jul 14 15:20:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:12:31 up  4:50,  1 user,  load average: 0.01, 0.14, 0.12

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -83,6 +83,7 @@ void NDL_OpenCanvas(int *w, int *h) {
 }
 void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
   int fd =open("/proc/dispinfo",O_RDWR);
+  printf("NDL_DrawRect w is %d h is %d\n",canvas_w,canvas_h);
   int offset = (y >=0 ? y:0)*canvas_w + (x>=0 ? x: 0);
   int len;
   if(x>=0){
