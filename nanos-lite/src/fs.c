fe8c899512e9dfd0d39f31ad24a3b20165479270
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-11 11:16:25 +0800

    >  compile NEMU ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.15.0-43-generic #46~20.04.1-Ubuntu SMP Thu Jul 14 15:20:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:16:25 up  2:45,  1 user,  load average: 0.92, 0.37, 0.20

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -122,9 +122,9 @@ size_t fs_write( int  fd, const void * buf,size_t count){
     case FD_STDERR:{
       for (int i = 0; i < count; i++){
         putch(((char*)(buf))[i]);
-        break;
       }
-      printf("fs_write count = %d\n",count);
+        break;
+      // printf("fs_write count = %d\n",count);
     }
       
     case FD_FB:{
