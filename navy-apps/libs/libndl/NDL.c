737fa1eaaf6cd2ded0a3d493174babd9906384d9
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-11 14:06:53 +0800

    >  compile NEMU ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.15.0-43-generic #46~20.04.1-Ubuntu SMP Thu Jul 14 15:20:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:06:53 up  4:45,  1 user,  load average: 0.30, 0.27, 0.13

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -48,8 +48,8 @@ void NDL_OpenCanvas(int *w, int *h) {
   char buf[128] = {0};
   read(fd,&buf,sizeof(buf));
   printf("buf[100] = %s\n",buf);
-  //sscanf(buf,"%*[^:]:%*[ ]%d\n%*[^:]:%*[ ]%d\n",&canvas_w,&canvas_h);
-  sscanf(buf,"%d %d" ,&canvas_w,&canvas_h);
+  sscanf(buf,"%*[^:]:%*[ ]%d\n%*[^:]:%*[ ]%d\n",&canvas_w,&canvas_h);
+  //sscanf(buf,"%d %d" ,&canvas_w,&canvas_h);
   //sprintf(buf, "%d %d", canvas_w, screen_h);
   printf("NDL_OpenCanvas w is %d h is %d\n",canvas_w,canvas_h);
  //printf("NDL\n");
