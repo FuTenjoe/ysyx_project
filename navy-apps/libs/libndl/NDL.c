1a5e0f1fff79d3c9f1ef06a3f38fc88212477ed6
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-11 14:07:42 +0800

    >  compile NEMU ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.15.0-43-generic #46~20.04.1-Ubuntu SMP Thu Jul 14 15:20:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:07:42 up  4:46,  1 user,  load average: 0.30, 0.28, 0.14

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -97,7 +97,7 @@ void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
     //printf("drawing2\n");
     if(y+i>=0 && y+i < canvas_h){
       lseek(fd,offset + cnt_r*canvas_w,SEEK_SET);
-      printf("drwing\n");
+      //printf("drwing\n");
       write(fd,pixels+i*w,len);
       cnt_r++;
     }
