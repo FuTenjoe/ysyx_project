990d58b6244850e1230c4945718816a498959152
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-11 21:22:54 +0800

    >  compile NEMU ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.15.0-43-generic #46~20.04.1-Ubuntu SMP Thu Jul 14 15:20:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:22:54 up 11:22,  1 user,  load average: 0.12, 0.13, 0.09

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -16,8 +16,11 @@ void SDL_FillRect(SDL_Surface *dst, SDL_Rect *dstrect, uint32_t color) {
 void SDL_UpdateRect(SDL_Surface *s, int x, int y, int w, int h) {
   //add
   if(s->format->BitsPerPixel == 32){
+    int s_w = s->w,s_h = s->h;
+    if(w==0 | w > s_w) w = s_w;
+    if(h==0 | h > s_h) h = s_h;
     NDL_DrawRect((uint32_t*)s->pixels, x, y, w, h);
-    printf("equal to before\n");
+    printf("s->w = %d s->h = %d",s->w,s->h);
   }
   else{
     int s_w = s->w,s_h = s->h;
