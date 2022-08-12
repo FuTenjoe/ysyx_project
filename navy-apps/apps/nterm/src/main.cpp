256b7fa5827f58856cdaf0bfb96602a2a33978cf
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-12 21:25:21 +0800

    >  compile NEMU ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:25:21 up  4:29,  1 user,  load average: 0.20, 0.20, 0.22

--- navy-apps/apps/nterm/src/main.cpp
+++ navy-apps/apps/nterm/src/main.cpp
@@ -18,9 +18,8 @@ int main(int argc, char *argv[]) {
   int win_w = font->w * W;
   int win_h = font->h * H;
   screen = SDL_SetVideoMode(win_w, win_h, 32, SDL_HWSURFACE);
-  printf("sdl_init sdl_vidieomode ok\n");
   term = new Terminal(W, H);
-
+  printf("sdl_init sdl_vidieomode ok\n");
   if (argc < 2) { builtin_sh_run(); }
   else { extern_app_run(argv[1]); }
 
