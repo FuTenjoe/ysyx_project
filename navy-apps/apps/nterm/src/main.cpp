dd05c82cebfff88d13f0c74243fc5758e9b6df6a
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-12 21:31:24 +0800

    >  compile NEMU ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:31:24 up  4:35,  1 user,  load average: 0.19, 0.15, 0.18

--- navy-apps/apps/nterm/src/main.cpp
+++ navy-apps/apps/nterm/src/main.cpp
@@ -24,7 +24,7 @@ int main(int argc, char *argv[]) {
   if (argc < 2) { builtin_sh_run(); }
   else { extern_app_run(argv[1]); }
   // should not reach here
-  assert(0);
+  //assert(0);
 }
 
 static void draw_ch(int x, int y, char ch, uint32_t fg, uint32_t bg) {
