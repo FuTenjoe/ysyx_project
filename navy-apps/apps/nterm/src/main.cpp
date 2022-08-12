4a547769584aa309235bf6d63347198cac2e70dc
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-12 22:01:22 +0800

    >  compile NEMU ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:01:22 up  5:05,  1 user,  load average: 0.22, 0.26, 0.20

--- navy-apps/apps/nterm/src/main.cpp
+++ navy-apps/apps/nterm/src/main.cpp
@@ -47,8 +47,9 @@ void refresh_terminal() {
   //printf("clear\n");
   static uint32_t last = 0;
   static int flip = 0;
+  printf("Before SDL_GetTicks\n");
   uint32_t now = SDL_GetTicks();
-  printf("refresh_terminal\n");
+  printf("After SDL_GetTicks\n");
   if (now - last > 500 || needsync) {
     int x = term->cursor.x, y = term->cursor.y;
     uint32_t color = (flip ? term->foreground(x, y) : term->background(x, y));
