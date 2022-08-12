11518a87c78e916e79de5ee9572c3c9afa57591f
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-12 20:52:02 +0800

    >  compile NEMU ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:52:02 up  3:56,  1 user,  load average: 0.09, 0.19, 0.17

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -29,7 +29,7 @@ int SDL_PollEvent(SDL_Event *ev) {
     ev->type = SDL_USEREVENT;
   }
   else{
-    char* str = key + 3;
+  /*  char* str = key + 3;
     printf("SDL_PollEvent %s %d\n",key,strlen(key));
     for(int i=0,cnt=0; i<sizeof(keyname);i+=strlen(keyname[cnt]),cnt++){
       if(!strncmp(keyname[cnt],str,strlen(str))&&!strncmp(keyname[cnt],str,strlen(keyname[cnt]))){  //比较设定大小的字符串相同返回0
@@ -48,7 +48,30 @@ int SDL_PollEvent(SDL_Event *ev) {
       keystate[keycode] = 1;
     }
     else {assert(0);}
-    return 1;
+    return 1;*/
+     if(key[0]=='k' && key[1]=='u')
+        {ev->type = SDL_KEYUP;}
+    else if(key[0]=='k' && key[1]=='d')
+        { ev->type = SDL_KEYDOWN;}
+    else{assert(0);}
+    char buf2[16];
+    strcpy(buf2,key+3);
+    buf2[strlen(buf2)-1]='\0';
+
+    for(int i =0;i<83 ;i++){
+      //printf("%d [%s]||||[%s]||||\n",i,keyname[i],buf2);
+      if(strcmp((const char*)buf2,(const char*)keyname[i]) == 0){
+        ev->key.keysym.sym = i;
+        //printf("%s",buf2);
+
+        keystate[i]= (ev->type == SDL_KEYDOWN);
+        return 1;
+
+      }
+    }
+
+
+
   }
   }
   
