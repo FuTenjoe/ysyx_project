d193a5417a83aef866a0893856f139982409c061
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-14 22:11:11 +0800

    >  compile NEMU ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:11:11 up  6:32,  1 user,  load average: 1.25, 0.82, 0.68

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -64,9 +64,9 @@ int SDL_PeepEvents(SDL_Event *ev, int numevents, int action, uint32_t mask) {
 }
 
 uint8_t* SDL_GetKeyState(int *numkeys) {
- if(numkeys == NULL){
+/* if(numkeys == NULL){
   return keystate;
  }
- else assert(0);
- //return keystate;
+ else assert(0);*/
+ return keystate;
 }
