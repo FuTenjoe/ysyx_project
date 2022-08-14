d295a1f9a92adbd02af8767c6ce1c8088ab38c7f
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-14 15:09:46 +0800

    >  compile NEMU ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:09:46 up  9:40,  1 user,  load average: 0.24, 0.34, 0.25

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -127,7 +127,7 @@ void SDL_UpdateRect(SDL_Surface *s, int x, int y, int w, int h) {
       }
     }
    
-    //NDL_DrawRect((uint32_t*)palette,x,y,w,h);
+    NDL_DrawRect((uint32_t*)selfpalette,x,y,w,h);
     free(selfpalette);
 
   }
