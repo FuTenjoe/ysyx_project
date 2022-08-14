94602e79ccfb0220c68dd89eca1c79b334d4c44b
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-14 20:44:56 +0800

    >  compile NEMU ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:44:56 up  5:06,  1 user,  load average: 0.40, 0.38, 0.34

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -70,11 +70,11 @@ size_t fb_write(const void *buf, size_t offset, size_t len) {
   int w = io_read(AM_GPU_CONFIG).width;
   //int h = io_read(AM_GPU_CONFIG).height;
   //printf("fb_write w=%d,h=%d\n",w,h);
-  int x = ((offset )/4 )%w;
+  int x = ((offset )/4 )%287;
   int y = ((offset )/4)/w;
   //io_write(AM_GPU_FBDRAW,x,y,(uint32_t*)buf,len/4,1,true);
   io_write(AM_GPU_FBDRAW,x,y,(uint32_t*)buf,len/4,1,true);
-  printf("len = %d\n",len);
+  //printf("len = %d\n",len);
   return len;
   
 }
