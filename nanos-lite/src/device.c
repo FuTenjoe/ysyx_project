f6bcfa582dffc51cfadab44008e739b547ce2410
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-14 20:17:56 +0800

    >  compile NEMU ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:17:56 up  4:39,  1 user,  load average: 0.41, 0.36, 0.28

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -72,6 +72,7 @@ size_t fb_write(const void *buf, size_t offset, size_t len) {
   //printf("fb_write w=%d,h=%d\n",w,h);
   int x = ((offset )/4 )%w;
   int y = ((offset )/4)/w;
+  //io_write(AM_GPU_FBDRAW,x,y,(uint32_t*)buf,len/4,1,true);
   io_write(AM_GPU_FBDRAW,x,y,(uint32_t*)buf,len/4,1,true);
   //printf("len = %d\n",len);
   return len;
--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -102,7 +102,7 @@ void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
     len = (x + w < canvas_w) ? x+w : canvas_w;
   }
   //printf("len= %d\n",len);
-  int  cnt_r = 0;
+/*  int  cnt_r = 0;
   for(int i=0; i<h; i++){
     //printf("drawing2\n");
     if(y+i>=0 && y+i < canvas_h){
@@ -113,8 +113,15 @@ void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
       cnt_r++;
     }
     else printf("error");
-  }
+  }*/
   //printf("Finish DRAWRECT!\n");
+for(int i=0; i<h; i++){
+    //printf("drawing2\n");
+    for(int j=0; i<w; j++){
+      lseek(fd,offset*4 +4,SEEK_SET);
+      write(fd,pixels+i*canvas_w+j,4);
+    }
+  }
   close(fd);
 }
 
