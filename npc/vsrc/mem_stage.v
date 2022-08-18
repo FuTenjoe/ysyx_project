978e323ff70e0bcc6c4678a81861b017f40836f5
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-18 22:27:54 +0800

    >  sim RTL ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:27:54 up  9:33,  1 user,  load average: 0.00, 0.04, 0.07

--- npc/vsrc/mem_stage.v
+++ npc/vsrc/mem_stage.v
@@ -26,7 +26,7 @@ module mem_stage(
     output [2:0] mem_rd_buf_flag
     
 );
- reg [2:0] reg_rd_buf_flag
+reg [2:0] reg_rd_buf_flag;
 wire [63:0] rd_buf_lw;
 ///assign rd_buf_lw = (present_state == FN)?axi_rdata:64'd0;
 reg [63:0] alu_res;
