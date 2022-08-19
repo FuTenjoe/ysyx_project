9841dc016d4bd8f4b490453cdd677fb187b26ece
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-19 10:00:27 +0800

    >  sim RTL ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:00:27 up  1:11,  1 user,  load average: 0.14, 0.28, 0.18

--- npc/vsrc/wb_stage.v
+++ npc/vsrc/wb_stage.v
@@ -36,7 +36,6 @@ always@(*)begin
 end
 
 always @(*) begin
-   
     if (rst_n && reg_wen && (reg_waddr != `REG_ADDR_WIDTH'b0)&&(s_flag==1'd0))begin // x0 read only
             case(expand_signed)
             4'd0:begin
