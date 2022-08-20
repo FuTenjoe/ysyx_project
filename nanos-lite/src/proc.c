e57b275adcc8a23291218939a89e4467c0a48d9a
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-20 18:33:42 +0800

    >  sim RTL ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:33:42 up  7:38,  1 user,  load average: 0.53, 0.27, 0.18

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -24,8 +24,8 @@ void init_proc() {
   Log("Initializing processes...");
  // naive_uload(NULL, "/bin/event-test");
  //naive_uload(NULL, "/bin/pal");
- // naive_uload(NULL, "/bin/hello");
- naive_uload(NULL, NULL);
+  naive_uload(NULL, "/bin/dummy");
+
 }
 
 Context* schedule(Context *prev) {
