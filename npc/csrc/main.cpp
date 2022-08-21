96d7e8be72fbadcc037280fa893e2bd1f9ce6bc0
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-21 21:16:31 +0800

    >  sim RTL ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:16:31 up  1:00,  1 user,  load average: 0.16, 0.10, 0.04

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -374,10 +374,10 @@ static void checkregs(CPU_state *ref, vaddr_t dnpc){
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t dnpc){
   int i = 0;
   bool DIF_result = true;
-/*  if(top->inst== 115){
+  if(top->inst== 115){
     printf("ecall\n");
     assert(1);
-  }*/
+  }
 /*  if(ref_r -> pc != dnpc){
     printf("False: PC is false! ref_dnpc is 0x%0lx;npc_dnpc is 0x%0lx; Instruction is 0x%x\n",ref_r->pc,dnpc,top->inst);
     DIF_result = false;
