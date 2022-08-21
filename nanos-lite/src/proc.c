51c0b6e7738158902800fdbfb9be776735c415dc
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-21 12:22:49 +0800

    >  compile NEMU ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:22:49 up  2:08,  1 user,  load average: 0.18, 0.06, 0.02

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -24,7 +24,7 @@ void init_proc() {
   Log("Initializing processes...");
  // naive_uload(NULL, "/bin/event-test");
  //naive_uload(NULL, "/bin/pal");
-  naive_uload(NULL, "/bin/dummy");
+  naive_uload(NULL, "/bin/hello");
 
 }
 
