d527a3db127854a2354bfda81d57b6efe2a3faab
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-21 18:39:18 +0800

    >  sim RTL ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:39:18 up  8:19,  1 user,  load average: 0.10, 0.11, 0.07

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -72,7 +72,7 @@ extern "C" void pmem_read(long long raddr,long long *rdata){
   if(raddr >= CONFIG_MBASE){
     *rdata = host_read(guest_to_host(raddr), 8);
     //printf("raddr = 0x%llx\n",raddr);
-    printf("rdata = 0x%llx\n",*rdata);
+   // printf("rdata = 0x%llx\n",*rdata);
   }
   else{
     printf("raddr = %llx\n",raddr);
