263675af10b52b449a5aaa9f879ecdbcc1269169
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-21 16:13:32 +0800

    >  sim RTL ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:13:32 up  5:59,  1 user,  load average: 0.12, 0.08, 0.03

--- abstract-machine/am/src/riscv/npc/cte.c
+++ abstract-machine/am/src/riscv/npc/cte.c
@@ -7,7 +7,19 @@ Context* __am_irq_handle(Context *c) {
   if (user_handler) {
     Event ev = {0};
     switch (c->mcause) {
-      default: ev.event = EVENT_ERROR; break;
+      case(11):{
+        if((c->gpr[17] >=0) &&(c->gpr[17] <=19)){
+          ev.event = EVENT_SYSCALL;break;
+        }
+        if(c->gpr[17] == -1){
+          ev.event = EVENT_YIELD;break;
+        }
+        else {
+          ev.event = EVENT_ERROR;break;
+        }
+      }
+      //case(11):ev.event = EVENT_YIELD;break;
+      default: ev.event = EVENT_ERROR;break;
     }
 
     c = user_handler(ev, c);
