600f8aafaa5b60d2f9b06df65328fab89dc0a327
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-26 16:51:11 +0800

    >  sim RTL ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:51:11 up 11:18,  1 user,  load average: 0.03, 0.10, 0.08

--- npc/vsrc/id_rest.v
+++ npc/vsrc/id_rest.v
@@ -109,7 +109,7 @@ always @(*) begin
         cunqu_hazard = 1'b0;
     end
 end
-wire test2 = (wb_reg_waddr == 2'd0) ?1'b0:1'b1;
+wire test2 = (reg1_raddr == wb_reg_waddr || reg2_raddr == wb_reg_waddr) ?1'b0:1'b1;
 
 
 
