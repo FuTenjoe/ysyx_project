f2bddcab07f0d6d767e7a5d683657058c7c9b1c6
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-26 20:20:41 +0800

    >  sim RTL ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:20:41 up 14:46,  1 user,  load average: 0.47, 0.18, 0.06

--- npc/vsrc/i_cache.v
+++ npc/vsrc/i_cache.v
@@ -150,19 +150,19 @@ always@(posedge clk)begin
 	if(state==CompareTag && hit)begin
 		if(hit1)begin
 			cpu_ready<=1'b1;
-			cpu_data_read<=cache_data[2*cpu_req_index][64*cpu_req_offset[4:2] +:64];
+			cpu_data_read<=cache_data[2*cpu_req_index][64*cpu_req_offset[3:2] +:64];
 		end
 		else begin
 			cpu_ready<=1'b1;
-			cpu_data_read<=cache_data[2*cpu_req_index+1][64*cpu_req_offset[4:2] +:64];
+			cpu_data_read<=cache_data[2*cpu_req_index+1][64*cpu_req_offset[3:2] +:64];
 		end
 	end
 	else if(state==CompareTag2 && hit)begin
 		cpu_ready<=1'b1;
 			if(hit1)
-				cpu_data_read<=cache_data[2*cpu_req_index][64*delay_cpu_req_offset[4:2] +:64];
+				cpu_data_read<=cache_data[2*cpu_req_index][64*delay_cpu_req_offset[3:2] +:64];
 			else
-				cpu_data_read<=cache_data[2*cpu_req_index+1][64*delay_cpu_req_offset[4:2] +:64];
+				cpu_data_read<=cache_data[2*cpu_req_index+1][64*delay_cpu_req_offset[3:2] +:64];
 	end
 	else begin 
 		cpu_ready<=1'b0;
