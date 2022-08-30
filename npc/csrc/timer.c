c95ad2951ef8a666b4040b3402286f9a3667539b
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-30 10:03:15 +0800

    >  sim RTL ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:03:15 up 10 min,  1 user,  load average: 0.05, 0.09, 0.07

--- npc/csrc/timer.c
+++ npc/csrc/timer.c
@@ -2,13 +2,20 @@
 //#include <device/alarm.h>
 //#include <utils.h>
 //自己加
-
 #include <stdio.h>
 #include <stdlib.h>
+#include "Vysyx_22040175_top.h"
 #include "assert.h"
 #include <dlfcn.h>  //动态链接库相关函数
 
+enum{DIFFTEST_TO_DUT,DIFFTEST_TO_REF,NPC_STOP,NPC_RUNNING,NPC_END,NPC_ABORT};
+typedef uint64_t word_t;
+typedef  uint32_t paddr_t;
+typedef  word_t vaddr_t;
+typedef unsigned long int	uintptr_t;
+int npc_state = NPC_STOP; 
 
+//自己加结束
 static uint32_t *rtc_port_base = NULL;
 
 static void rtc_io_handler(uint32_t offset, int len, bool is_write) {
@@ -22,7 +29,7 @@ static void rtc_io_handler(uint32_t offset, int len, bool is_write) {
 
 #ifndef CONFIG_TARGET_AM
 static void timer_intr() {
-  if (nemu_state.state == NEMU_RUNNING) {
+  if (npc_state == NPC_RUNNING) {
     extern void dev_raise_intr();
     dev_raise_intr();
   }
