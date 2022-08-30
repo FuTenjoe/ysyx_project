91763bacb93282d6a2b5c608244b75de676274b0
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-30 11:06:24 +0800

    >  sim RTL ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:06:24 up  1:13,  1 user,  load average: 0.06, 0.15, 0.09

--- npc/csrc/timer.c
+++ npc/csrc/timer.c
@@ -87,11 +87,6 @@ typedef struct {
 static IOMap maps[NR_MAP] = {};
 static int nr_map = 0;
 
-static inline IOMap* fetch_mmio_map(paddr_t addr) {
-  int mapid = find_mapid_by_addr(maps, nr_map, addr);
-  return (mapid == -1 ? NULL : &maps[mapid]);
-}
-
 
 void add_mmio_map(const char *name, paddr_t addr,
         void *space, uint32_t len, io_callback_t callback);
