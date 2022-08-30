ea60923f14ef833692309fe0106fb9080843be64
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-30 10:43:40 +0800

    >  sim RTL ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:43:40 up 51 min,  1 user,  load average: 0.07, 0.03, 0.00

--- npc/include/npc.h
+++ npc/include/npc.h
@@ -1,3 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "Vysyx_22040175_top.h"
+#include "assert.h"
+#include <dlfcn.h>  //动态链接库相关函数
 
 typedef void(*io_callback_t)(uint32_t, int, bool);
 uint8_t* new_space(int size);
