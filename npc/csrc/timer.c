1b0ae1fbc6c2b40ecbe0418a8594fbe52b498c9b
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-30 11:08:07 +0800

    >  sim RTL ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:08:07 up  1:15,  1 user,  load average: 0.12, 0.15, 0.09

--- npc/csrc/timer.c
+++ npc/csrc/timer.c
@@ -118,7 +118,7 @@ void init_timer() {
 #ifdef CONFIG_HAS_PORT_IO
   add_pio_map ("rtc", CONFIG_RTC_PORT, rtc_port_base, 8, rtc_io_handler);
 #else
-  //add_mmio_map("rtc", 0xa1000048, rtc_port_base, 8, rtc_io_handler);
+  add_mmio_map("rtc", 0xa1000048, rtc_port_base, 8, rtc_io_handler);
 #endif
   add_alarm_handle(timer_intr);
 }
