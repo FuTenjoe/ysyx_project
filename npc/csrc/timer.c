bcf40e979740e8ac12f9fbb6a75796cee9b1c489
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-31 11:14:26 +0800

    >  sim RTL ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:14:26 up 58 min,  1 user,  load average: 0.04, 0.03, 0.00

--- npc/csrc/timer.c
+++ npc/csrc/timer.c
@@ -1,6 +1,6 @@
-#include <device/map.h>
-#include <device/alarm.h>
-#include <utils.h>
+//#include <device/map.h>
+//#include <device/alarm.h>
+//#include <utils.h>
 
 static uint32_t *rtc_port_base = NULL;
 
