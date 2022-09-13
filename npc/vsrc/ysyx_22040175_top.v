2c41074517a97ffe6fe045a656438016aa515fe7
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-13 15:21:03 +0800

    >  sim RTL ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:21:03 up  4:13,  1 user,  load average: 0.00, 0.00, 0.00

--- npc/vsrc/ysyx_22040175_top.v
+++ npc/vsrc/ysyx_22040175_top.v
@@ -38,8 +38,8 @@ cpu u_cpu(
 
 
     .r_done2(r_done2),
-    .axi_r_ready_o2(axi_r_ready_o2),   //总线变化
-    .axi_ar_id_o2(axi_ar_id_o2),   //
+    .axi_r_ready_o2(r_ready),   //总线变化
+    .axi_ar_id_o2(ar_id),   //
     //.axi_r_ready_o2(r_ready),   //总线变化
    // .axi_ar_id_o2(ar_id),   //
     .rdata(rdata),
@@ -75,7 +75,7 @@ wire [63:0] axi_r_addr;
 
 
 wire axi_aw_ready_i;
-wire axi_aw_valid_o;
+wire axi_aw_valid_o; 
 wire [63:0] axi_aw_addr_o;
 wire axi_w_ready_i;
 wire axi_w_valid_o;
@@ -103,7 +103,7 @@ wire [7:0] reg_write_wmask;
 wire rst_n = !rst;
 
 //移植接口
-wire aw_ready;
+    wire aw_ready;
     wire aw_valid;
     wire [`AXI_ADDR_WIDTH-1:0] aw_addr;
     wire [2:0] aw_prot;
@@ -152,11 +152,6 @@ wire aw_ready;
     wire [`AXI_ID_WIDTH-1:0] r_id;
     wire [`AXI_USER_WIDTH-1:0] r_user;
 
-
-
-
-
-
 axi # (
 )
 u_axi2(
@@ -175,32 +170,32 @@ u_axi2(
     .ww_addr_i(reg_write_addr),
 
 
-    .axi_ar_ready_i(axi_ar_ready_i2),                
-    .axi_ar_valid_o(axi_ar_valid_o2),
-    .axi_ar_addr_o(axi_ar_addr_o2),
-    .axi_ar_id_o(axi_ar_id_o2),
-    .axi_ar_len_o(axi_ar_len_o2),
-    .axi_ar_size_o(axi_ar_size_o2),
-    .axi_ar_burst_o(axi_ar_burst_o2),
-    .axi_r_ready_o(axi_r_ready_o2),                 
-    .axi_r_valid_i(axi_r_valid_i2),                
-    .axi_r_resp_i(axi_r_resp_i2),
-    .axi_r_data_i(axi_r_data_i2),
-    .axi_r_last_i(axi_r_last_i2),
+    .axi_ar_ready_i(ar_ready),                
+    .axi_ar_valid_o(ar_valid),
+    .axi_ar_addr_o(ar_addr),
+    .axi_ar_id_o(ar_id),
+    .axi_ar_len_o(ar_len),
+    .axi_ar_size_o(ar_size),
+    .axi_ar_burst_o(ar_burst),
+    .axi_r_ready_o(r_ready),                 
+    .axi_r_valid_i(r_valid),                
+    .axi_r_resp_i(r_resp),
+    .axi_r_data_i(r_data),
+    .axi_r_last_i(r_last),
     .r_done(r_done2),
     .axi_r_id_i(send_axi_ar_id),
 
-    .axi_aw_ready_i(axi_aw_ready_i),    //从设备已准备好接收地址和相关的控制信号          
-    .axi_aw_valid_o(axi_aw_valid_o),  
-    .axi_aw_addr_o(axi_aw_addr_o),
+    .axi_aw_ready_i(aw_ready),    //从设备已准备好接收地址和相关的控制信号          
+    .axi_aw_valid_o(aw_valid),  
+    .axi_aw_addr_o(aw_addr),
 
-    .axi_w_ready_i(axi_w_ready_i),                
-    .axi_w_valid_o(axi_w_valid_o),
-    .axi_w_data_o(axi_w_data_o),
-    .axi_w_strb_o(axi_w_strb_o),
-    .axi_w_last_o(axi_w_last_o),
-    .axi_b_ready_o(axi_b_ready_o),                
-    .axi_b_valid_i(axi_b_valid_i),
+    .axi_w_ready_i(w_ready),                
+    .axi_w_valid_o(w_valid),
+    .axi_w_data_o(w_data),
+    .axi_w_strb_o(w_strb),
+    .axi_w_last_o(w_last),
+    .axi_b_ready_o(b_ready),                
+    .axi_b_valid_i(b_valid),
 
 
     .ar_hs(ar_hs),
@@ -210,113 +205,6 @@ u_axi2(
 
 );
 
-
-axi_slave # (
-)
-u_axi_slave2(
-    .clock(clk),
-    .reset_n(rst_n),
-    .axi_ar_ready_o(axi_ar_ready_i2),    //从设备已准备好接收地址和相关的控制信号            
-    .axi_ar_valid_i(axi_ar_valid_o2),
-    .axi_ar_addr_i(axi_ar_addr_o2),
-    .axi_ar_len_i(axi_ar_len_o2), //突发长度，这个字段标识每次突发传输的传输次数
-    .axi_ar_size_i(axi_ar_size_o2),  //突发大小，这个字段表示每次突发传输的大小
-    .axi_ar_burst_i(axi_ar_burst_o2),  //突发类型，包括突发类型和突发大小信息，该字段决定了每次突发传输时地址的计算方法
-    .axi_r_ready_i(axi_r_ready_o2),   //  	主设备已准备好接收读取的数据和响应信息              
-    .axi_r_valid_o(axi_r_valid_i2),  //从设备给出的数据和响应信息有效              
-    .axi_r_resp_o(axi_r_resp_i2), //读响应，这信号表示读传输的状态
-    .axi_r_data_o(axi_r_data_i2),
-    .axi_r_last_o(axi_r_last_i2),  //该信号用于标识当前传输是否为突发传输中的最后一次传输
-    .r_valid(axi_valid | waxi_valid),
-
-    .axi_req(cache_axi_req),
-    
-    .axi_aw_ready_o(axi_aw_ready_i),    //从设备已准备好接收地址和相关的控制信号          
-    .axi_aw_valid_i(axi_aw_valid_o),  
-    .axi_aw_addr_i(axi_aw_addr_o),
-
-    //.axi_aw_addr_i(test),
-    .axi_w_ready_o(axi_w_ready_i),                
-    .axi_w_valid_i(axi_w_valid_o),
-    .axi_w_data_i(axi_w_data_o),
-    .axi_w_strb_i(axi_w_strb_o),
-    .axi_w_last_i(axi_w_last_o),
-    .axi_b_ready_i(axi_b_ready_o),                
-    .axi_b_valid_o(axi_b_valid_i)
-);
-
-/*
-axi_yuan u_axi_rw (
-        .clock(clk),
-        .reset_n(rst_n),
-        .rw_req_i(cache_axi_req),
-        .rw_mask(reg_write_wmask),
-
-        .rw_valid_i(axi_valid | waxi_valid),         //IF&MEM输入信号
-        
-        .data_read_o(rdata),        //IF&MEM输入信号
-        .rw_w_data_i(reg_write_data),        //IF&MEM输入信号
-        .rw_addr_i(axi_r_addr),          //IF&MEM输入信号
-        
-        .rw_burst(axi_burst),
-        .ww_addr_i(reg_write_addr),
-
-
-
-        .axi_aw_ready_i                 (aw_ready),
-        .axi_aw_valid_o                 (aw_valid),
-        .axi_aw_addr_o                  (aw_addr),
-    //    .axi_aw_prot_o                  (aw_prot),
-    //    .axi_aw_id_o                    (aw_id),
-    //    .axi_aw_user_o                  (aw_user),
-    //    .axi_aw_len_o                   (aw_len),
-    //    .axi_aw_size_o                  (aw_size),
-    //    .axi_aw_burst_o                 (aw_burst),
-    //    .axi_aw_lock_o                  (aw_lock),
-    //    .axi_aw_cache_o                 (aw_cache),
-    //    .axi_aw_qos_o                   (aw_qos),
-    //    .axi_aw_region_o                (aw_region),
-
-        .axi_w_ready_i                  (w_ready),
-        .axi_w_valid_o                  (w_valid),
-        .axi_w_data_o                   (w_data),
-        .axi_w_strb_o                   (w_strb),
-        .axi_w_last_o                   (w_last),
-    //    .axi_w_user_o                   (w_user),
-        
-        .axi_b_ready_o                  (b_ready),
-        .axi_b_valid_i                  (b_valid),
-    //    .axi_b_resp_i                   (b_resp),
-    //    .axi_b_id_i                     (b_id),
-    //    .axi_b_user_i                   (b_user),
-
-        .axi_ar_ready_i                 (ar_ready),
-        .axi_ar_valid_o                 (ar_valid),
-        .axi_ar_addr_o                  (ar_addr),
-    //    .axi_ar_prot_o                  (ar_prot),
-        .axi_ar_id_o                    (ar_id),
-    //    .axi_ar_user_o                  (ar_user),
-        .axi_ar_len_o                   (ar_len),
-        .axi_ar_size_o                  (ar_size),
-        .axi_ar_burst_o                 (ar_burst),
-    //    .axi_ar_lock_o                  (ar_lock),
-    //    .axi_ar_cache_o                 (ar_cache),
-    //    .axi_ar_qos_o                   (ar_qos),
-    //   .axi_ar_region_o                (ar_region),
-        
-        .axi_r_ready_o                  (r_ready),
-        .axi_r_valid_i                  (r_valid),
-        .axi_r_resp_i                   (r_resp),
-        .axi_r_data_i                   (r_data),
-        .axi_r_last_i                   (r_last),
-        .axi_r_id_i                     (r_id),
-    //    .axi_r_user_i                   (r_user),
-
-        .r_done(r_done2),
-        .w_done(w_done),
-        .b_hs(b_hs)
-
-    );
 
 axi_slave # (
 )
@@ -336,8 +224,6 @@ u_axi_slave2(
     .axi_r_last_o(r_last),  //该信号用于标识当前传输是否为突发传输中的最后一次传输
     .r_valid(axi_valid | waxi_valid),
 
-
-
     .axi_req(cache_axi_req),
     
     .axi_aw_ready_o(aw_ready),    //从设备已准备好接收地址和相关的控制信号          
@@ -352,7 +238,5 @@ u_axi_slave2(
     .axi_w_last_i(w_last),
     .axi_b_ready_i(b_ready),                
     .axi_b_valid_o(b_valid)
-
 );
-*/
 endmodule
