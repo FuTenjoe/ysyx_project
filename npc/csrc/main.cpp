0ba7ddadf91ed09da92be654824613721e5411c8
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-13 12:20:19 +0800

    >  sim RTL ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:20:19 up  2:03,  1 user,  load average: 1.05, 0.45, 0.28

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -191,7 +191,7 @@ int main(int argc, char **argv, char **env) {
       //top->inst = pmem_read(top->pc,8); //使用DPIC
       
       top->eval ();
-   /*   printf("main_time = %d\n",i);
+      printf("main_time = %d\n",i);
       printf("PC:0x%0x;Inst:0x%x;\n",top->pc,top->inst);
       //printf("ena=:0x%0x, top->ena);
       printf(" a0= 0x%08lx; Instruction is 0x%x\n",cpu_gpr[10],top->inst);
@@ -200,7 +200,7 @@ int main(int argc, char **argv, char **env) {
       printf(" s0= 0x%08lx; Instruction is 0x%x\n",cpu_gpr[8],top->inst);
       printf(" a2= 0x%08lx; Instruction is 0x%x\n",cpu_gpr[18],top->inst);
       printf(" a5= 0x%08lx; Instruction is 0x%x\n",cpu_gpr[15],top->inst);
-      printf("mem_rd_buf_flag is 0x%08lx\n",top->out_mem_rd_buf_flag);*/
+      printf("mem_rd_buf_flag is 0x%08lx\n",top->out_mem_rd_buf_flag);
      }
      if(top->clk==0){
         
