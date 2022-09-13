012a91431b2c83379fe06ecf69371374b360dc0c
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-13 11:25:33 +0800

    >  sim RTL ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:25:33 up  1:08,  0 users,  load average: 0.16, 0.74, 0.55

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -213,7 +213,7 @@ int main(int argc, char **argv, char **env) {
        printf(" ra= 0x%08lx; Instruction is 0x%x\n",cpu_gpr[1],top->inst);
       printf(" a5= 0x%08lx; Instruction is 0x%x\n",cpu_gpr[15],top->inst);
       printf(" s0= 0x%08lx; Instruction is 0x%x\n",cpu_gpr[8],top->inst);
-      printf("mem_rd_buf_flag is 0x%08lx\n",top->out_mem_rd_buf_flag);  
+      
       //init_difftest(img_size,port);
       if (a>1){
         //if (a%4==1 & a/4>=1){
